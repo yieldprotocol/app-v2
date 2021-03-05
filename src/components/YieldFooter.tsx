47b5695b89ad12a1ac8afb54a15d300e39a019e3
brucedonovan <bruce.donovan@gmail.com>
2021-03-05 10:24:22 +0000

    feat: footer with test panel

---
+++ src/components/YieldFooter.tsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react';
+import {
+  Text,
+  Box,
+  Button,
+  ResponsiveContext,
+  Footer,
+  Collapsible,
+} from 'grommet';
+import { toast } from 'react-toastify';
+import { ChainContext } from '../contexts/ChainContext';
+
+const YieldFooter = (props: any) => {
+  const mobile:boolean = (useContext<any>(ResponsiveContext) === 'small');
+  const { state: chainState, actions } = useContext(ChainContext);
+  const [testOpen, setTestOpen] = useState<boolean>(false);
+
+  return (
+    <Footer pad="small">
+
+      <Box gap="medium" fill="horizontal">
+
+        <Box onClick={() => setTestOpen(!testOpen)} alignSelf="end">
+          <Text size="xsmall"> {testOpen ? 'close test panel' : 'open test panel'} </Text>
+        </Box>
+
+        <Collapsible open={testOpen}>
+          <Box direction="row" gap="small">
+            <Button type="button" onClick={() => actions.connect('injected')} label="Connect web3" />
+            <Button type="button" onClick={() => actions.disconnect()} label="Disconnect web3" />
+            <p>{chainState.account}</p>
+            <p>{chainState.chainId}</p>
+            <Button primary onClick={() => toast('Transaction complete')} label="Notify Example" />
+          </Box>
+        </Collapsible>
+
+      </Box>
+
+    </Footer>
+  );
+};
+
+export default YieldFooter;
