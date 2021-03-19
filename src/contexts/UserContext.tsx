082e4c005ae7cf5b33b9f9679951ee33cf2c967e
brucedonovan <bruce.donovan@gmail.com>
2021-03-19 15:11:17 +0000

    feat: userContext vault building from events

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -2,20 +2,18 @@ import React, { useState, useContext, useEffect, useReducer, useCallback } from 
 import { ethers } from 'ethers';
 import { toast } from 'react-toastify';
 
-import { IYieldAsset, IYieldSeries, IYieldVault, IYieldUser } from '../types';
+import { IYieldAsset, IYieldSeries, IYieldVault } from '../types';
 
 import { ChainContext } from './ChainContext';
 
 const UserContext = React.createContext<any>({});
 
 const initState = {
-  // vaultMap: new Map() as Map<string, IYieldVault>,
-  vaultMap: null as Map<string, IYieldVault> | null, // for testing only
-  activeVault: null as IYieldUser | null,
+  vaultMap: new Map() as Map<string, IYieldVault>,
 
+  activeVault: null as IYieldVault | null,
   activeSeries: null as IYieldSeries | null,
   activeAsset: null as IYieldAsset | null,
-  activeUser: null as IYieldUser | null,
 
 };
 
@@ -42,13 +40,38 @@ function userReducer(state:any, action:any) {
 }
 
 const UserProvider = ({ children }:any) => {
-  const [vaultState, updateState] = useReducer(userReducer, initState);
+  const [userState, updateState] = useReducer(userReducer, initState);
 
-  const { chainState: { account } } = useContext(ChainContext);
+  const { chainState: { contractMap, account, chainLoading } } = useContext(ChainContext);
 
   useEffect(() => {
-    account && console.log('okay, go!!!!');
-  }, [account]);
+    /* when chainContext is finsihed Loading get the vaults */
+    account && !chainLoading && (async () => {
+      const Cauldron = contractMap.get('Cauldron');
+      const events = await Cauldron.filters.VaultBuilt(null, account, null);
+      const eventList = await Cauldron.queryFilter(events);
+      const vaultList = await Promise.all(eventList.map(async (x:any) => {
+        const { vaultId: id, ilkId, seriesId } = Cauldron.interface.parseLog(x).args;
+        /* Add in any extra static asset Data */
+        return {
+          id,
+          seriesId,
+          ilkId,
+        };
+      }));
+      const newVaultMap = vaultList.reduce((acc:any, item:any) => {
+        const _map = acc;
+        _map.set(item.id, item);
+        return _map;
+      }, userState.vaultMap);
+      updateState({ type: 'vaultMap', payload: newVaultMap });
+    })();
+  }, [account, chainLoading, contractMap, userState.vaultMap]);
+
+  /* subscribe to vault event listeners */
+  useEffect(() => {
+
+  }, []);
 
   const vaultActions = {
     setActiveSeries: (series:IYieldSeries) => updateState({ type: 'activeSeries', payload: series }),
@@ -56,7 +79,7 @@ const UserProvider = ({ children }:any) => {
   };
 
   return (
-    <UserContext.Provider value={{ vaultState, vaultActions }}>
+    <UserContext.Provider value={{ userState, vaultActions }}>
       {children}
     </UserContext.Provider>
   );
