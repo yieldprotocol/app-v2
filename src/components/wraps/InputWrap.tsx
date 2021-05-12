cd19de59359a12bd32a28aec8226b1463499f053
brucedonovan <bruce.donovan@gmail.com>
2021-05-12 17:45:16 +0100

    feat: error inputbox

--- src/components/wraps/InputWrap.tsx
+++ src/components/wraps/InputWrap.tsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { Box, BoxProps, ResponsiveContext } from 'grommet';
+import { Box, BoxProps, Text, ResponsiveContext } from 'grommet';
 
 interface IInputWrap extends BoxProps {
   action?: ()=>void;
   disabled?: boolean;
+  isError?:string|null;
   children: any;
 }
 
@@ -11,20 +12,25 @@ function InputWrap({ action, disabled, children, ...props }: IInputWrap) {
   const mobile:boolean = useContext<any>(ResponsiveContext) === 'small';
 
   return (
-    <Box
-      {...props}
-      direction="row"
-      round="xsmall"
-      border
-      pad={{ horizontal: 'small' }}
-      align="center"
-      basis={mobile ? '50%' : '65%'}
-    >
-      { children }
+    <Box>
+      <Box
+        {...props}
+        direction="row"
+        round="xsmall"
+        border
+        pad={{ horizontal: 'small' }}
+        align="center"
+        basis={mobile ? '50%' : '65%'}
+        background={props.isError ? 'pink' : undefined}
+      >
+        { children }
+      </Box>
+      <Text color="pink" size="xsmall"> {props.isError} </Text>
     </Box>
+
   );
 }
 
-InputWrap.defaultProps = { action: () => null, disabled: false };
+InputWrap.defaultProps = { action: () => null, disabled: false, isError: null };
 
 export default InputWrap;
