b12c2159495d76bab9549f5c3ec1a4692aed20d0
brucedonovan <bruce.donovan@gmail.com>
2021-05-13 14:58:38 +0100

    feat: seperate out pool actions

--- src/views/Pool.tsx
+++ src/views/Pool.tsx
@@ -1,5 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Box, Button, RadioButtonGroup, ResponsiveContext, Text, TextInput } from 'grommet';
+
+import { ethers } from 'ethers';
 
 import { cleanValue } from '../utils/displayUtils';
 import AssetSelector from '../components/selectors/AssetSelector';
@@ -8,39 +10,77 @@ import SeriesSelector from '../components/selectors/SeriesSelector';
 import InputWrap from '../components/wraps/InputWrap';
 import InfoBite from '../components/InfoBite';
 import ActionButtonGroup from '../components/ActionButtonGroup';
-import PlaceholderWrap from '../components/wraps/PlaceholderWrap';
 import SectionWrap from '../components/wraps/SectionWrap';
 import { UserContext } from '../contexts/UserContext';
 import { ISeries, IUserContext } from '../types';
 import { usePoolActions } from '../hooks/poolActions';
+import MaxButton from '../components/MaxButton';
 
 function Pool() {
   const mobile:boolean = useContext<any>(ResponsiveContext) === 'small';
-  const [inputValue, setInputValue] = useState<any>(undefined);
-  const [strategy, setStrategy] = useState<'BUY'|'MINT'>('BUY');
-  const [rollToSeries, setRollToSeries] = useState<ISeries|null>(null);
 
   /* state from context */
   const { userState } = useContext(UserContext) as IUserContext;
-  const { seriesMap, selectedSeriesId, selectedBaseId } = userState;
-
+  const { activeAccount, assetMap, seriesMap, selectedSeriesId, selectedBaseId } = userState;
   const selectedSeries = seriesMap.get(selectedSeriesId!);
+  const selectedBase = assetMap.get(selectedBaseId!);
 
+  /* local state */
+  const [poolInput, setPoolInput] = useState<string>();
+  const [removeInput, setRemoveInput] = useState<string>();
+  const [rollInput, setRollInput] = useState<string>();
+
+  const [rollToSeries, setRollToSeries] = useState<ISeries|null>(null);
+
+  const [maxPool, setMaxPool] = useState<string|undefined>();
+  const [maxRemove, setMaxRemove] = useState<string|undefined>();
+
+  const [poolError, setPoolError] = useState<string|null>(null);
+  const [removeError, setRemoveError] = useState<string|null>(null);
+  const [rollError, setRollError] = useState<string|null>(null);
+
+  const [poolDisabled, setPoolDisabled] = useState<boolean>(true);
+  const [removeDisabled, setRemoveDisabled] = useState<boolean>(true);
+  const [rollDisabled, setRollDisabled] = useState<boolean>(true);
+
+  const [strategy, setStrategy] = useState<'BUY'|'MINT'>('BUY');
+
+  /* import hook fns */
   const { addLiquidity, removeLiquidity, rollLiquidity } = usePoolActions();
+
+  /* Check max available to pool */
+  useEffect(() => {
+    activeAccount &&
+          (async () => {
+            /* Checks asset selection and sets the max available value */
+            const max = await selectedBase?.getBalance(activeAccount);
+            if (max) setMaxPool(ethers.utils.formatEther(max).toString());
+          })();
+  }, [activeAccount, poolInput, selectedBase, setMaxPool]);
+
+  /* Check max available to remove/close */
+  useEffect(() => {
+    activeAccount &&
+            (async () => {
+              /* Checks asset selection and sets the max available value */
+              const max = await selectedBase?.getBalance(activeAccount);
+              if (max) setMaxPool(ethers.utils.formatEther(max).toString());
+            })();
+  }, [activeAccount, removeInput, selectedBase, setMaxRemove]);
 
   const handleAdd = () => {
     // !lendDisabled &&
-    selectedSeries && addLiquidity(inputValue, selectedSeries, strategy);
+    selectedSeries && addLiquidity(poolInput, selectedSeries, strategy);
   };
 
   const handleRemove = () => {
     // !lendDisabled &&
-    selectedSeries && removeLiquidity('1', selectedSeries);
+    selectedSeries && removeLiquidity(removeInput, selectedSeries);
   };
 
-  const handleRollLiquidity = () => {
+  const handleRoll = () => {
     // !lendDisabled &&
-    selectedSeries && rollToSeries && rollLiquidity(inputValue, selectedSeries, rollToSeries);
+    selectedSeries && rollToSeries && rollLiquidity(rollInput, selectedSeries, rollToSeries);
   };
 
   return (
@@ -51,16 +91,14 @@ function Pool() {
             <TextInput
               plain
               type="number"
-              placeholder={<PlaceholderWrap label="Enter amount" />}
-              value={inputValue || ''}
-              onChange={(event:any) => setInputValue(cleanValue(event.target.value))}
+              placeholder="Enter Amount"
+              value={poolInput || ''}
+              onChange={(event:any) => setPoolInput(cleanValue(event.target.value))}
             />
-            {
-              !mobile &&
-              <Box onClick={() => console.log('max clicked ')} pad="xsmall">
-                <Text size="xsmall" color="text">MAX</Text>
-              </Box>
-            }
+            <MaxButton
+              action={() => setPoolInput(maxPool)}
+              disabled={maxPool === '0'}
+            />
           </InputWrap>
           <Box basis={mobile ? '50%' : '35%'}>
             <AssetSelector />
@@ -99,19 +137,51 @@ function Pool() {
       <ActionButtonGroup buttonList={[
         <Button
           primary
-          label={<Text size={mobile ? 'small' : undefined}> {`Pool ${inputValue || ''} Dai`}</Text>}
+          label={<Text size={mobile ? 'small' : undefined}> {`Pool ${poolInput || ''} Dai`}</Text>}
           key="primary"
           onClick={() => handleAdd()}
+        />,
 
-        />,
-        <Button
-          secondary
-          label={<Text size={mobile ? 'small' : undefined}> Remove Liquidity </Text>}
-          key="secondary"
-          onClick={() => handleRemove()}
-        />,
       ]}
       />
+
+      <SectionWrap
+        title="Remove Liquidity:"
+        border={{
+          color: 'grey',
+          style: 'dashed',
+          side: 'all',
+        }}
+      >
+        <Box direction="row" gap="small" fill="horizontal" align="center">
+          <InputWrap action={() => console.log('maxAction')}>
+            <TextInput
+              plain
+              type="number"
+              placeholder="Enter tokens to remove"
+              value={removeInput || ''}
+              onChange={(event:any) => setRemoveInput(cleanValue(event.target.value))}
+            />
+            <MaxButton
+              action={() => setRemoveInput(maxRemove)}
+              disabled={maxRemove === '0'}
+            />
+          </InputWrap>
+        </Box>
+
+        <Box gap="small" fill="horizontal" direction="row" align="center">
+          <ActionButtonGroup buttonList={[
+            <Button
+              primary
+              label={<Text size={mobile ? 'small' : undefined}> Remove </Text>}
+              key="primary"
+              onClick={() => handleRemove()}
+            />,
+          ]}
+          />
+        </Box>
+
+      </SectionWrap>
 
       <SectionWrap
         title="Roll Liquidity to:"
@@ -121,8 +191,23 @@ function Pool() {
           side: 'all',
         }}
       >
-        <Box gap="small" fill="horizontal" direction="row" align="center">
+        <Box direction="row" gap="small" fill="horizontal" align="center">
+          <InputWrap action={() => console.log('maxAction')}>
+            <TextInput
+              plain
+              type="number"
+              placeholder="Tokens to remove"
+              value={rollInput || ''}
+              onChange={(event:any) => setRollInput(cleanValue(event.target.value))}
+            />
+            <MaxButton
+              action={() => setRollInput(maxRemove)}
+              disabled={maxRemove === '0'}
+            />
+          </InputWrap>
+        </Box>
 
+        <Box gap="small" fill="horizontal" direction="row" align="center">
           <SeriesSelector selectSeriesLocally={(series:ISeries) => setRollToSeries(series)} />
 
           <Box basis="35%">
@@ -131,7 +216,7 @@ function Pool() {
                 primary
                 label={<Text size={mobile ? 'small' : undefined}> Roll </Text>}
                 key="primary"
-                onClick={() => handleRollLiquidity()}
+                onClick={() => handleRoll()}
               />,
             ]}
             />
