a0eb8ee209c1eb9463bdc8f0f134e0fb9bffb51b
brucedonovan <bruce.donovan@gmail.com>
2021-05-28 15:16:07 +0100

    feat: ONE as const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,12 +3,16 @@ import { ethers, BigNumber } from 'ethers';
 /* constants */
 export const MAX_256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 export const MAX_128 = '0xffffffffffffffffffffffffffffffff';
+export const ZERO_BN = ethers.constants.Zero;
+export const ONE_BN = ethers.constants.One;
+// export const MINUS_ONE_BN = ethers.constants.One.mul(-1);
+
+export const ONE_RAY_BN = BigNumber.from('1000000000000000000000000000');
+export const ONE_WEI_BN = BigNumber.from('1000000000000000000');
 
 export const SECONDS_PER_YEAR: number = (365 * 24 * 60 * 60);
 export const ETH_BYTES = ethers.utils.formatBytes32String('ETH-A');
 export const CHAI_BYTES = ethers.utils.formatBytes32String('CHAI');
-export const ONE_RAY_BN = BigNumber.from('1000000000000000000000000000');
-export const ONE_WEI_BN = BigNumber.from('1000000000000000000');
 
 export const WETH = '0x455448000000';
 export const DAI = '0x444149000000';
