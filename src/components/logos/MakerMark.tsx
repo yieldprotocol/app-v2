16ca2b8c762240a7b5bfa5de348add724d4db01e
brucedonovan <bruce.donovan@gmail.com>
2021-06-04 19:02:57 +0100

    feat: logos and marks

---
+++ src/components/logos/MakerMark.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function MakerMark(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 351 180" fill="none" {...props}>
+      <path
+        d="M27.339 180V38.805l107.077 80.58V180h27.338v-66.299c0-5.01-2.354-9.73-6.358-12.742L25.536 3.234C15.028-4.675 0 2.823 0 15.977V180h27.339zM323.48 180V38.805l-107.077 80.58V180h-27.338v-66.299c0-5.01 2.354-9.73 6.358-12.742L325.282 3.234c10.51-7.909 25.537-.411 25.537 12.743V180H323.48z"
+        fill="#1AAB9B"
+      />
+    </svg>
+  );
+}
+
+export default MakerMark;
