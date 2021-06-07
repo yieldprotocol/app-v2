3ab3fd790f0fb7a2ae2b80848b3ba9d129bb6613
brucedonovan <bruce.donovan@gmail.com>
2021-06-07 12:00:18 +0100

    refactor: remove vaultWrap

--- src/components/ActionButtonGroup.tsx
+++ src/components/ActionButtonGroup.tsx
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react';
-import { Box, Layer, ResponsiveContext } from 'grommet';
+import { Button, Box, Text, Layer, ResponsiveContext } from 'grommet';
 import styled from 'styled-components';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import { ChainContext } from '../contexts/ChainContext';
 
 function ActionButtonGroup({ ...props }:any) {
   const mobile:boolean = (useContext<any>(ResponsiveContext) === 'small');
+
+  const { chainState: { account }, chainActions: { connect } } = useContext(ChainContext);
 
   return (
     mobile ? (
@@ -20,7 +24,13 @@ function ActionButtonGroup({ ...props }:any) {
       </Layer>)
       :
       <Box gap="small" fill="horizontal">
-        {props.children}
+        { account
+          ? props.children
+          : <Button
+              secondary
+              label={<Text size={mobile ? 'small' : undefined}> Connect Wallet </Text>}
+              onClick={() => connect()}
+          />}
       </Box>
   );
 }
