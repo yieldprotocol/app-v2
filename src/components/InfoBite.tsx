b0098c29b50baa944f05554a31fb22c473fa5dff
brucedonovan <bruce.donovan@gmail.com>
2021-06-15 14:46:22 +0200

    feat: infobite update

--- src/components/InfoBite.tsx
+++ src/components/InfoBite.tsx
@@ -8,9 +8,9 @@ interface IInfoBite {
 const InfoBite = ({ label, value }: IInfoBite) => {
   const mobile:boolean = useContext<any>(ResponsiveContext) === 'small';
   return (
-    <Box direction={mobile ? 'row' : undefined} justify={mobile ? 'between' : undefined}>
-      <Text size={mobile ? 'xsmall' : 'small'} color="text-weak" style={{ textDecoration: 'underline' }}> {label}</Text>
-      <Text size={mobile ? 'xsmall' : 'small'} color="text-weak"> {value}</Text>
+    <Box direction="row" justify={mobile ? 'between' : undefined} gap="small">
+      <Text weight="bold" size={mobile ? 'xsmall' : 'small'} color="text-weak"> {label}</Text>
+      <Text size={mobile ? 'xsmall' : 'small'} color="text"> {value}</Text>
     </Box>);
 };
 
