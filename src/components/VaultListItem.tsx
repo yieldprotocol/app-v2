02c4b35cd7a406af4d42fd550721d4e6c8c38a52
brucedonovan <bruce.donovan@gmail.com>
2021-06-29 13:12:32 +0200

    feat: imporved vaultList Item

--- src/components/VaultListItem.tsx
+++ src/components/VaultListItem.tsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-import { Box, Stack, Text } from 'grommet';
+import { Avatar, Box, Stack, Text } from 'grommet';
 import { IAsset, ISeries, IUserContext, IVault } from '../types';
 import { UserContext } from '../contexts/UserContext';
+import YieldMark from './logos/YieldMark';
 
 function VaultListItem({ vault }:{ vault:IVault }) {
   const { userState } = useContext(UserContext) as IUserContext;
@@ -17,13 +18,30 @@ function VaultListItem({ vault }:{ vault:IVault }) {
       gap="small"
       align="center"
       pad="small"
-      background={series?.color}
       round="xsmall"
+      // style={{
+      //   backgroundImage: `url('data:image/svg+xml;utf8,' + 'svg')`,
+      // }}
     >
-      <Box direction="row" pad="xsmall" round="large" background={`linear-gradient(90deg, ${base?.color} 40%, #ffffff00 75%)`} gap="xsmall">
-        {base?.image}
-        {ilk?.image}
-      </Box>
+      <Stack anchor="top-right">
+        <Avatar background={series?.color}>
+          {/* <Box direction="row" pad="xsmall" round="large" background={`linear-gradient(90deg, ${base?.color} 40%, #ffffff00 75%)`} gap="xsmall">
+          {base?.image}
+          {ilk?.image}
+        </Box> */}
+          <Box
+            round="large"
+            background={base?.color}
+            pad="xsmall"
+            align="center"
+          >
+            {base?.image}
+          </Box>
+        </Avatar>
+        <Avatar background="#fff" size="xsmall">
+          {ilk?.image}
+        </Avatar>
+      </Stack>
 
       <Box>
         <Text weight={900} size="small"> {vault.displayName} </Text>
