d2e3c4bf2e1adff3764da48c522ad32ff4b3dc5c
brucedonovan <bruce.donovan@gmail.com>
2021-07-05 12:47:29 +0100

    feat: account image/personality

---
+++ src/components/YieldAccount.tsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  Text,
+  Box,
+  ResponsiveContext,
+  Layer,
+  Avatar,
+} from 'grommet';
+import {
+  FiSettings,
+  FiMenu,
+} from 'react-icons/fi';
+
+import YieldBalances from './YieldBalances';
+
+import { ChainContext } from '../contexts/ChainContext';
+import { abbreviateHash } from '../utils/displayUtils';
+import YieldAvatar from './YieldAvatar';
+
+const YieldAccount = (props: any) => {
+  const mobile:boolean = (useContext<any>(ResponsiveContext) === 'small');
+  const { chainState: { account, chainId }, chainActions: { connect, disconnect } } = useContext(ChainContext);
+
+  const [settingsOpen, setSettingsOpen] = useState<boolean>();
+
+  return (
+    <>
+      { settingsOpen &&
+      <Layer
+        onClickOutside={() => setSettingsOpen(false)}
+      >
+        <Box> Settings </Box>
+      </Layer>}
+
+      {account ?
+        <Box direction="row" fill="vertical" gap="xsmall">
+          {
+              !mobile &&
+              <>
+                <YieldBalances />
+              </>
+          }
+          <Box round="xsmall" onClick={() => setSettingsOpen(true)} pad="small" justify="center">
+            {mobile ?
+              <Text size="small" color="text">
+                <FiSettings />
+              </Text>
+              :
+              <Box direction="row" align="center" gap="small">
+                <Box>
+                  <Text color="text" size="small">
+                    {abbreviateHash(account)}
+                  </Text>
+                  <Text size="xsmall" color="text-weak">
+                    o Connected
+                  </Text>
+                </Box>
+
+                <Box>
+                  <YieldAvatar address={account.concat('y')} size={2.5} />
+                </Box>
+
+                {/* <FiSettings /> */}
+              </Box>}
+          </Box>
+        </Box>
+        :
+        <Box border={!mobile} onClick={() => connect()} pad="small">
+          <Text size="small" color="text"> { mobile ? <FiMenu /> : 'Connect Wallet'} </Text>
+        </Box>}
+    </>
+  );
+};
+
+export default YieldAccount;
--- src/components/YieldHeader.tsx
+++ src/components/YieldHeader.tsx
@@ -4,19 +4,16 @@ import React, { useContext, useState, useRef } from 'react';
 import { FiMenu, FiToggleRight, FiToggleLeft } from 'react-icons/fi';
 import { ChainContext } from '../contexts/ChainContext';
 import { TxContext } from '../contexts/TxContext';
-import Balances from './Balances';
 import YieldLogo from './logos/YieldLogo';
 
 import YieldNavigation from './YieldNavigation';
+import YieldAccount from './YieldAccount';
 
 interface IYieldHeaderProps {
   actionList: any[];
 }
 const YieldHeader = ({ actionList } : IYieldHeaderProps) => {
   const mobile:boolean = useContext<any>(ResponsiveContext) === 'small';
-  const { chainState: { account, chainId }, chainActions: { connect, disconnect } } = useContext(ChainContext);
-  const { txState: { txPending, signPending, processPending } } = useContext(TxContext);
-  const [toggleMenu] = actionList;
 
   return (
 
@@ -28,32 +25,11 @@ const YieldHeader = ({ actionList } : IYieldHeaderProps) => {
       style={{ position: 'fixed', top: '0px' }}
       background="background"
     >
-      <YieldLogo height={mobile ? '1em' : '1.5em'} />
+      <YieldLogo height={mobile ? '1em' : '2em'} />
 
       { !mobile && <YieldNavigation /> }
 
-      {
-        account ?
-          <Box direction="row" fill="vertical" gap="xsmall">
-            {
-                !mobile &&
-                <>
-                  <Balances />
-                  {/* <Box pad="small">
-                    <Text size="xsmall" color="green"> Connected to: {chainId} </Text>
-                    <Box onClick={() => disconnect()}> <Text size="xsmall" color="text-xweak"> Disconnect </Text> </Box>
-                  </Box> */}
-                </>
-            }
-            <Box round="xsmall" border={!mobile} onClick={() => toggleMenu()} pad="small" justify="center">
-              <Text size="small" color="text"> { mobile ? <FiMenu /> : 'Account and Settings'} </Text>
-            </Box>
-          </Box>
-          :
-          <Box border={!mobile} onClick={() => connect()} pad="small">
-            <Text size="small" color="text"> { mobile ? <FiMenu /> : 'Connect Wallet'} </Text>
-          </Box>
-        }
+      <YieldAccount />
 
     </Header>
 
