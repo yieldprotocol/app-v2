0532e0b83e801d4855b76503930c27cc02a29e08
brucedonovan <bruce.donovan@gmail.com>
2021-07-26 10:30:05 +0100

    feat: copy wrap

---
+++ src/components/wraps/CopyWrap.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Box } from 'grommet';
+import { 
+  FiCheckCircle as Check,
+  FiCopy as Copy,
+} from 'react-icons/fi';
+import { copyToClipboard } from '../../utils/appUtils';
+
+
+const CopyWrap = ({ children, hash } : any) => {
+
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const copy = (e:any) => {
+    e.stopPropagation();
+    setCopied(true);
+    copyToClipboard(hash);
+  };
+
+  useEffect(()=>{
+    copied && (async () => setTimeout(
+      ()=> setCopied(false),
+      5000
+    ))();
+  }, [copied]);
+
+  return (
+    <Box direction='row' gap='small' align='center'>
+      {children} 
+      <Box
+        onClick={(e:any)=> copy(e)}
+      >
+        {copied? <Check /> : <Copy /> }
+      </Box>
+    </Box>
+  );
+};
+
+export default CopyWrap;
