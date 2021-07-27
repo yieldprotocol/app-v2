7cd0352b403bdc7e49dcb8e7251f6794f9041abc
marcomariscal <marco.a.mariscal@gmail.com>
2021-07-27 18:59:36 -0400

    changing times color on modal wrap

--- src/components/wraps/ModalWrap.tsx
+++ src/components/wraps/ModalWrap.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Box, Header, Layer, ResponsiveContext } from 'grommet';
+import { Box, Button, Header, Layer, ResponsiveContext } from 'grommet';
 
 import { FiX } from 'react-icons/fi';
 import MainViewWrap from './MainViewWrap';
@@ -15,52 +15,57 @@ interface IModalWrap {
   background?: string | undefined;
 }
 
-
 function ModalWrap({ children, toggleModalOpen, background, modalOpen = false }: IModalWrap) {
   const mobile: boolean = useContext<any>(ResponsiveContext) === 'small';
 
-  const { userState: { selectedSeriesId, seriesMap } } = useContext(UserContext);
+  const {
+    userState: { selectedSeriesId, seriesMap },
+  } = useContext(UserContext);
 
   const series = seriesMap.get(selectedSeriesId);
 
   return (
     <Box>
-        {modalOpen && (
-          <Layer
-            // plain
-            onClickOutside={() => toggleModalOpen()}
-            responsive
-            full
-            background={background}
-            animation="none"
+      {modalOpen && (
+        <Layer
+          // plain
+          onClickOutside={() => toggleModalOpen()}
+          responsive
+          full
+          background={background}
+          animation="none"
+        >
+          <Header
+            pad="medium"
+            height={mobile ? undefined : 'xsmall'}
+            justify="between"
+            fill="horizontal"
+            style={{ position: 'fixed', top: '0px' }}
           >
-            <Header
-              pad="medium"
-              height={mobile ? undefined : 'xsmall'}
-              justify="between"
-              fill="horizontal"
-              style={{ position: 'fixed', top: '0px' }}
-            >
-              <YieldLogo height={mobile ? '1em' : '1.5em'} startColor={series?.oppStartColor} endColor={series?.oppEndColor} />
-              <FiX onClick={() => toggleModalOpen()} />
-            </Header>
+            <YieldLogo
+              height={mobile ? '1em' : '1.5em'}
+              startColor={series?.oppStartColor}
+              endColor={series?.oppEndColor}
+            />
+            <Button icon={<FiX onClick={() => toggleModalOpen()} color={series?.oppStartColor} />} />
+          </Header>
 
-            <Box flex={!mobile} overflow="auto" margin={{ top: 'xlarge' }}>
-              <MainViewWrap pad="large">
-                <PanelWrap>
-                  <Box />
-                </PanelWrap>
-                <Box gap="large" width="600px" pad={{ top: 'large' }}>
-                  {/* <BackButton action={() => toggleModalOpen()} /> */}
-                  {children}
-                </Box>
-                <PanelWrap>
-                  <Box />
-                </PanelWrap>
-              </MainViewWrap>
-            </Box>
-          </Layer>
-        )}
+          <Box flex={!mobile} overflow="auto" margin={{ top: 'xlarge' }}>
+            <MainViewWrap pad="large">
+              <PanelWrap>
+                <Box />
+              </PanelWrap>
+              <Box gap="large" width="600px" pad={{ top: 'large' }}>
+                {/* <BackButton action={() => toggleModalOpen()} /> */}
+                {children}
+              </Box>
+              <PanelWrap>
+                <Box />
+              </PanelWrap>
+            </MainViewWrap>
+          </Box>
+        </Layer>
+      )}
     </Box>
   );
 }
