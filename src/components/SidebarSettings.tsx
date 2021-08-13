34ab05079f3858feb86603494fbc8454dd3d9318
marcomariscal <marco.a.mariscal@gmail.com>
2021-08-13 18:05:42 -0400

    adding sidebar component to encapsulate settings

---
+++ src/components/SidebarSettings.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { Layer } from 'grommet';
+import { ChainContext } from '../contexts/ChainContext';
+import YieldSettings from './YieldSettings';
+import Connect from './Connect';
+
+const StyledLayer = styled(Layer)`
+  backdrop-filter: 0 !important;
+`;
+
+const SidebarSettings = ({ settingsOpen, setSettingsOpen, connectOpen, setConnectOpen }: any) => {
+  const {
+    chainState: { account },
+  } = useContext(ChainContext);
+
+  return (
+    <>
+      {connectOpen && (
+        <StyledLayer modal={false} responsive={true} full="vertical" position="right">
+          <Connect setConnectOpen={setConnectOpen} setSettingsOpen={setSettingsOpen} />
+        </StyledLayer>
+      )}
+
+      {account && settingsOpen && (
+        <StyledLayer modal={false} responsive={true} full="vertical" position="right">
+          <YieldSettings setConnectOpen={setConnectOpen} setSettingsOpen={setSettingsOpen} />
+        </StyledLayer>
+      )}
+    </>
+  );
+};
+
+export default SidebarSettings;
