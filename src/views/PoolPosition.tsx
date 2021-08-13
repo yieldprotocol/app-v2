aff08340344aa0bb798d642f59897fa33b920f02
marcomariscal <marco.a.mariscal@gmail.com>
2021-08-13 11:10:52 -0400

    removing extra button

--- src/views/PoolPosition.tsx
+++ src/views/PoolPosition.tsx
@@ -105,21 +105,19 @@ const PoolPosition = ({ close }: { close: () => void }) => {
   }, [activeAccount, removeError, removeInput, rollError, rollInput, rollToSeries]);
 
   /* INTERNAL COMPONENTS */
-  const CompletedTx = (props: any) =>(
+  const CompletedTx = (props: any) => (
     <>
-    <NextButton
-      // size="xsmall"
-      label={<Text size={mobile ? 'xsmall' : undefined}>Go back</Text>}
-      onClick={() => {
-        props.resetTx();
-        handleStepper(true);
-      }}
-    />
-    {props.tx.failed &&
-    <EtherscanButton txHash={props.tx.txHash} />
-    }
-  </>
-);
+      <NextButton
+        // size="xsmall"
+        label={<Text size={mobile ? 'xsmall' : undefined}>Go back</Text>}
+        onClick={() => {
+          props.resetTx();
+          handleStepper(true);
+        }}
+      />
+      {/* {props.tx.failed && <EtherscanButton txHash={props.tx.txHash} />} */}
+    </>
+  );
 
   return (
     <CenterPanelWrap>
@@ -286,25 +284,24 @@ const PoolPosition = ({ close }: { close: () => void }) => {
           />
         )}
 
-        {actionActive.index === 0 && stepPosition[actionActive.index] !== 0 && (
-          <TransactButton
-            primary
-            label={
-              <Text size={mobile ? 'small' : undefined}>
-                {`Remov${removeTx.processActive ? 'ing' : 'e'} ${
-                  nFormatter(Number(removeInput), selectedBase?.digitFormat!) || ''
-                } tokens`}
-              </Text>
-            }
-            onClick={() => handleRemove()}
-            disabled={removeDisabled || removeTx.processActive}
-          />
-        )}
+        {actionActive.index === 0 &&
+          stepPosition[actionActive.index] !== 0 &&
+          !(removeTx.success || removeTx.failed) && (
+            <TransactButton
+              primary
+              label={
+                <Text size={mobile ? 'small' : undefined}>
+                  {`Remov${removeTx.processActive ? 'ing' : 'e'} ${
+                    nFormatter(Number(removeInput), selectedBase?.digitFormat!) || ''
+                  } tokens`}
+                </Text>
+              }
+              onClick={() => handleRemove()}
+              disabled={removeDisabled || removeTx.processActive}
+            />
+          )}
 
-        {actionActive.index === 1 && 
-        stepPosition[actionActive.index] !== 0 && 
-        
-        (
+        {actionActive.index === 1 && stepPosition[actionActive.index] !== 0 && (
           <TransactButton
             primary
             label={
@@ -328,7 +325,6 @@ const PoolPosition = ({ close }: { close: () => void }) => {
           actionActive.index === 1 &&
           !rollTx.processActive &&
           (rollTx.success || rollTx.failed) && <CompletedTx tx={rollTx} resetTx={resetRollTx} />}
-
       </ActionButtonGroup>
     </CenterPanelWrap>
   );
