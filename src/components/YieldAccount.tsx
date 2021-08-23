19c0b0cd3ff19dba5dac9e032d4f2ea3d12e2fd0
marcomariscal <marco.a.mariscal@gmail.com>
2021-08-23 17:36:37 -0400

    removing tx state

--- src/components/YieldAccount.tsx
+++ src/components/YieldAccount.tsx
@@ -3,13 +3,9 @@ import styled from 'styled-components';
 import { Text, Box, ResponsiveContext } from 'grommet';
 import { FiSettings } from 'react-icons/fi';
 import Skeleton from 'react-loading-skeleton';
-
-import YieldBalances from './YieldBalances';
-
 import { ChainContext } from '../contexts/ChainContext';
-import { TxContext } from '../contexts/TxContext';
-
 import { abbreviateHash } from '../utils/appUtils';
+import YieldBalances from './YieldBalances';
 import YieldAvatar from './YieldAvatar';
 import ConnectButton from './buttons/ConnectButton';
 import SidebarSettings from './SidebarSettings';
