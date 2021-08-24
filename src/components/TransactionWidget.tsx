3a7c81bcb03a5139cc0ef0eda444d29c1a27c5ed
marcomariscal <marco.a.mariscal@gmail.com>
2021-08-24 18:36:38 -0400

    updating transaction widget structure to include transaction items

--- src/components/TransactionWidget.tsx
+++ src/components/TransactionWidget.tsx
@@ -1,56 +1,27 @@
-import React from 'react';
-import { Box, Text, Spinner } from 'grommet';
-import { FiAlertTriangle, FiCheckCircle, FiClock, FiXCircle } from 'react-icons/fi';
-import { abbreviateHash } from '../utils/appUtils';
-import CopyWrap from './wraps/CopyWrap';
+import React, { useContext } from 'react';
+import { Box } from 'grommet';
+import { TxContext } from '../contexts/TxContext';
+import TransactionWidgetItem from './TransactionWidgetItem';
 
-const TransactionWidget = ({ tx }: any) => {
-  if (!tx.txCode) return null;
+const TransactionWidget = () => {
+  const { transactions_ } = useContext(TxContext);
+
+  const txListRender = [...transactions_.keys()].map((t) => {
+    const tx = transactions_.get(t);
+    return <TransactionWidgetItem tx={tx} key={tx.id} />;
+  });
 
   return (
     <Box
       round="xsmall"
       pad={{ horizontal: 'medium', vertical: 'xsmall' }}
-      elevation={tx.processActive || tx.success || tx.failed ? 'xsmall' : undefined}
+      elevation="xsmall"
       gap="xsmall"
       fill
       align="center"
       justify="center"
     >
-      {!tx.pending && tx.processActive && (
-        <Box direction="row" gap="medium" align="center">
-          <FiAlertTriangle />
-          <Box>
-            <Text size="small">Action Required</Text>
-            <Text size="xsmall">Check your wallet</Text>
-          </Box>
-        </Box>
-      )}
-
-      {tx.pending && (
-        <Box direction="row" gap="medium" align="center">
-          {/* <FiClock /> */}
-          <Spinner />
-          <Box>
-            <Text size="small">Transaction Pending</Text>
-            <Text size="xsmall">
-              <CopyWrap hash={tx.txHash}>{abbreviateHash(tx.txHash, 6)} </CopyWrap>
-            </Text>
-          </Box>
-        </Box>
-      )}
-      {tx.success && (
-        <Box direction="row" gap="small">
-          <FiCheckCircle />
-          <Text size="xsmall"> Transaction Complete </Text>
-        </Box>
-      )}
-      {tx.failed && (
-        <Box direction="row" gap="small">
-          <FiXCircle />
-          <Text size="xsmall"> Transaction Failed </Text>
-        </Box>
-      )}
+      {txListRender}
     </Box>
   );
 };
---
+++ src/components/TransactionWidgetItem.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Box, Text, Spinner } from 'grommet';
+import { FiAlertTriangle, FiCheckCircle, FiXCircle } from 'react-icons/fi';
+import { abbreviateHash } from '../utils/appUtils';
+import CopyWrap from './wraps/CopyWrap';
+
+const TransactionWidgetItem = ({ tx }: any) => (
+  <Box
+    round="xsmall"
+    pad={{ horizontal: 'medium', vertical: 'xsmall' }}
+    elevation={tx.processActive || tx.success || tx.failed ? 'xsmall' : undefined}
+    gap="xsmall"
+    fill
+    align="center"
+    justify="center"
+  >
+    {!tx.pending && tx.processActive && (
+      <Box direction="row" gap="medium" align="center">
+        <FiAlertTriangle />
+        <Box>
+          <Text size="small">Action Required</Text>
+          <Text size="xsmall">Check your wallet</Text>
+        </Box>
+      </Box>
+    )}
+
+    {tx.pending && (
+      <Box direction="row" gap="medium" align="center">
+        {/* <FiClock /> */}
+        <Spinner />
+        <Box>
+          <Text size="small">Transaction Pending</Text>
+          <Text size="xsmall">
+            <CopyWrap hash={tx.txHash}>{abbreviateHash(tx.txHash, 6)} </CopyWrap>
+          </Text>
+        </Box>
+      </Box>
+    )}
+    {tx.success && (
+      <Box direction="row" gap="small">
+        <FiCheckCircle />
+        <Text size="xsmall"> Transaction Complete </Text>
+      </Box>
+    )}
+    {tx.failed && (
+      <Box direction="row" gap="small">
+        <FiXCircle />
+        <Text size="xsmall"> Transaction Failed </Text>
+      </Box>
+    )}
+  </Box>
+);
+
+export default TransactionWidgetItem;
