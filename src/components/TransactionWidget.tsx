6f90f90b7047a9fd5e6b9ca8071adf1fddcf0d85
marcomariscal <marco.a.mariscal@gmail.com>
2021-08-26 17:24:20 -0400

    parsing out each Transaction into it's own component to be able to remove it

--- src/components/TransactionWidget.tsx
+++ src/components/TransactionWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Box, Text } from 'grommet';
 import { FiAlertTriangle } from 'react-icons/fi';
 import { TxContext } from '../contexts/TxContext';
@@ -10,7 +10,8 @@ const TransactionWidget = () => {
     txState: { processes, transactions },
   } = useContext(TxContext);
   const hasActiveProcess = [...processes.values()].some((p) => p.status === 'ACTIVE');
-  const isLastTxPending = [...transactions.values()][transactions.size - 1].status === TxState.PENDING;
+  const lastTx = [...transactions?.values()][transactions.size - 1];
+  const isLastTxPending = lastTx?.status === TxState.PENDING;
 
   return (
     <Box gap="xsmall">
--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,10 +1,7 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Text, Spinner } from 'grommet';
-import { FiCheckCircle, FiXCircle } from 'react-icons/fi';
+import { Box } from 'grommet';
 import { TxContext } from '../contexts/TxContext';
-import { TxState } from '../types';
-import EtherscanButton from './buttons/EtherscanButton';
+import Transaction from './Transaction';
 
 const Transactions = ({ ...props }) => {
   const {
@@ -13,39 +10,9 @@ const Transactions = ({ ...props }) => {
 
   return (
     <Box>
-      {[...transactions.values()].map((tx: any) => {
-        const { status, txCode, tx: t } = tx;
-        const action = txCode.split('_')[0];
-        const link = txCode.split('_')[1];
-        return (
-          <Box align="center" fill direction="row" gap="small" {...props} key={t.hash}>
-            <Box width="3rem">
-              {status === TxState.PENDING && <Spinner color="tailwind-blue" />}
-              {status === TxState.SUCCESSFUL && <FiCheckCircle size="1.5rem" color="#34D399" />}
-              {status === TxState.FAILED && <FiXCircle size="1.5rem" color="#F87171" />}
-            </Box>
-            <Box gap="small" align="center" direction="row" justify="between" fill="horizontal">
-              <Box direction="row" justify="start" align="start">
-                <Text size="small">{action}</Text>
-              </Box>
-              <Box direction="row" align="center">
-                {status === TxState.SUCCESSFUL && action !== 'Borrow' ? (
-                  <Link
-                    to={`${action !== 'Borrow' ? action.toLowerCase() : 'vault'}position/${link}`}
-                    style={{ textDecoration: 'none' }}
-                  >
-                    <Text size="xsmall" color="tailwind-blue" style={{ verticalAlign: 'middle' }}>
-                      View Position
-                    </Text>
-                  </Link>
-                ) : (
-                  <EtherscanButton txHash={t.hash} />
-                )}
-              </Box>
-            </Box>
-          </Box>
-        );
-      })}
+      {[...transactions.values()].map((tx: any) => (
+        <Transaction tx={tx} {...props} key={tx.tx.hash} />
+      ))}
     </Box>
   );
 };
