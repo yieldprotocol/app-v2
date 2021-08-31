19dcede72e660c309a000707a76f83ec810e92da
marcomariscal <marco.a.mariscal@gmail.com>
2021-08-31 15:57:19 -0400

    add link to vault position

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import { Link } from 'react-router-dom';
 import { Box, Text, Spinner } from 'grommet';
 import { FiCheckCircle, FiXCircle } from 'react-icons/fi';
 import { ActionCodes, TxState } from '../types';
 import EtherscanButton from './buttons/EtherscanButton';
+import { ChainContext } from '../contexts/ChainContext';
 
 const Transaction = ({ tx, removeOnComplete, ...props }: { tx: any; removeOnComplete?: boolean }) => {
-  const { status, txCode, tx: t, complete } = tx;
+  const {
+    chainState: { contractMap },
+  } = useContext(ChainContext);
+  const { status, txCode, tx: t, complete, receipt } = tx;
+
+  const [positionId, setPositionId] = useState(txCode.split('_')[1]);
+
   const action = txCode.split('_')[0];
-  const link = txCode.split('_')[1];
   const getLinkPathPrefix = (_action: string) => {
     switch (_action) {
       case ActionCodes.BORROW:
-        return 'vault';
+        return 'vaultposition';
       case ActionCodes.ADD_LIQUIDITY:
-        return 'pool';
+        return 'poolposition';
       default:
-        return _action.toLowerCase();
+        return `${_action.toLowerCase()}position`;
     }
   };
+
+  // get the vault id after successfull borrowing
+  useEffect(() => {
+    if (action === ActionCodes.BORROW && receipt) {
+      const cauldronAddr = contractMap.get('Cauldron').address;
+      const cauldronEvents = receipt?.events?.filter((e: any) => e.address === cauldronAddr)[0];
+      const vaultIdHex = cauldronEvents?.topics[1];
+      const vaultId = vaultIdHex.slice(0, 26);
+      setPositionId(vaultId);
+    }
+  }, [receipt, contractMap, action]);
 
   return removeOnComplete && complete ? null : (
     <Box align="center" fill direction="row" gap="small" {...props} key={t.hash}>
@@ -32,8 +50,8 @@ const Transaction = ({ tx, removeOnComplete, ...props }: { tx: any; removeOnComp
           <Text size="small">{action}</Text>
         </Box>
         <Box direction="row" align="center">
-          {status === TxState.SUCCESSFUL && action !== 'Borrow' ? (
-            <Link to={`${getLinkPathPrefix(action)}position/${link}`} style={{ textDecoration: 'none' }}>
+          {status === TxState.SUCCESSFUL ? (
+            <Link to={`${getLinkPathPrefix(action)}/${positionId}`} style={{ textDecoration: 'none' }}>
               <Text size="xsmall" color="tailwind-blue" style={{ verticalAlign: 'middle' }}>
                 View Position
               </Text>
