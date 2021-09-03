feab1fa3e374705bc205a85d2cf6c564f4d7e374
brucedonovan <bruce.donovan@gmail.com>
2021-09-03 17:27:25 +0100

    bugfix: leaving trace fytoken on post maturity clo

--- src/hooks/actionHooks/useClosePosition.ts
+++ src/hooks/actionHooks/useClosePosition.ts
@@ -4,7 +4,7 @@ import { ChainContext } from '../../contexts/ChainContext';
 import { UserContext } from '../../contexts/UserContext';
 import { ICallData, ISeries, ActionCodes, LadleActions, RoutedActions } from '../../types';
 import { getTxCode } from '../../utils/appUtils';
-import { buyBase, calculateSlippage } from '../../utils/yieldMath';
+import { buyBase, calculateSlippage, sellFYToken } from '../../utils/yieldMath';
 import { useChain } from '../useChain';
 
 /* Lend Actions Hook */
@@ -23,15 +23,13 @@ export const useClosePosition = () => {
 
     const { fyTokenAddress, poolAddress, seriesIsMature } = series;
 
-    const _inputAsFyToken = buyBase(
-      series.baseReserves, 
-      series.fyTokenReserves, 
-      _input, 
-      series.getTimeTillMaturity()
-    );
-    
-    const _inputAsFyTokenWithSlippage = calculateSlippage(
-      _inputAsFyToken,
+    // buy fyToken value ( after maturity  fytoken === base value )
+    const _fyTokenValueOfInput = seriesIsMature
+      ? _input
+      : buyBase(series.baseReserves, series.fyTokenReserves, _input, series.getTimeTillMaturity());
+
+    const _fyTokenValueOfInputWithSlippage = calculateSlippage(
+      _fyTokenValueOfInput,
       userState.slippageTolerance.toString(),
       true
     );
@@ -41,24 +39,24 @@ export const useClosePosition = () => {
         {
           target: series,
           spender: 'LADLE',
-          message: 'Signing ERC20 Token approval',
+          message: 'Allow Ladle to move your fyTokens',
           ignoreIf: false, // never ignore
         },
       ],
       txCode
     );
 
-    console.log('sereis mature: ', seriesIsMature);
+    console.log('series mature: ', seriesIsMature);
 
     const calls: ICallData[] = [
       ...permits,
-  
+
       {
         operation: LadleActions.Fn.TRANSFER,
         args: [
-          fyTokenAddress, 
-          seriesIsMature ? fyTokenAddress : poolAddress,  // select dest based on maturity 
-          _inputAsFyToken
+          fyTokenAddress,
+          seriesIsMature ? fyTokenAddress : poolAddress, // select dest based on maturity
+          _fyTokenValueOfInput,
         ] as LadleActions.Args.TRANSFER,
         ignoreIf: false, // never ignore
       },
@@ -66,7 +64,7 @@ export const useClosePosition = () => {
       /* BEFORE MATURITY */
       {
         operation: LadleActions.Fn.ROUTE,
-        args: [account, _inputAsFyTokenWithSlippage] as RoutedActions.Args.SELL_FYTOKEN,
+        args: [account, _fyTokenValueOfInputWithSlippage] as RoutedActions.Args.SELL_FYTOKEN,
         fnName: RoutedActions.Fn.SELL_FYTOKEN,
         targetContract: series.poolContract,
         ignoreIf: seriesIsMature,
@@ -75,10 +73,9 @@ export const useClosePosition = () => {
       /* AFTER MATURITY */
       {
         operation: LadleActions.Fn.REDEEM,
-        args: [series.id, account, _inputAsFyToken] as LadleActions.Args.REDEEM,
+        args: [series.id, account, _fyTokenValueOfInput] as LadleActions.Args.REDEEM,
         ignoreIf: !seriesIsMature,
       },
-
     ];
     await transact(calls, txCode);
     updateSeries([series]);
