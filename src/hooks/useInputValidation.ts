417e4b40e6b869b15ece66c30338a5091a7c51bc
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-07 17:28:44 -0400

    formatting

--- src/hooks/useInputValidation.ts
+++ src/hooks/useInputValidation.ts
@@ -24,7 +24,6 @@ export const useInputValidation = (
 
   useEffect(() => {
     if (activeAccount && (input || input === '')) {
-
       const _inputAsFloat = parseFloat(input);
       const aboveMax: boolean = !!limits[1] && _inputAsFloat > parseFloat(limits[1].toString());
       const belowMin: boolean = !!limits[0] && _inputAsFloat < parseFloat(limits[0].toString());
@@ -37,27 +36,26 @@ export const useInputValidation = (
       } else setInputError(null);
 
       // Action specific rules: - or message customising/Overriding:
-
       switch (actionCode) {
         case ActionCodes.BORROW:
           input &&
             selectedSeries &&
-            ethers.utils.parseUnits(input, selectedSeries.decimals ).gt(selectedSeries.baseReserves) &&
+            ethers.utils.parseUnits(input, selectedSeries.decimals).gt(selectedSeries.baseReserves) &&
             setInputError(`Amount exceeds the ${selectedBase?.symbol} currently available in pool`);
           aboveMax && setInputError('Exceeds the max allowable debt for this series');
-          belowMin && setInputError('A minimum debt of 0.5 is required');   
+          belowMin && setInputError('A minimum debt of 0.5 is required');
           break;
 
         case ActionCodes.REPAY:
         case ActionCodes.ROLL_DEBT:
-          
           /* set dust limit Error between 0 and dustLimit */
-          limits[0] && limits[1] &&
-          _inputAsFloat >= parseFloat(limits[0].toString()) &&
-          _inputAsFloat !== parseFloat(limits[1].toString()) &&
-          setInputError('Remaining debt below dust levels');
+          limits[0] &&
+            limits[1] &&
+            _inputAsFloat >= parseFloat(limits[0].toString()) &&
+            _inputAsFloat !== parseFloat(limits[1].toString()) &&
+            setInputError('Remaining debt below dust levels');
           /* set dustError between 0 and dustLimit */
-          aboveMax && setInputError('Amount exceeds your current debt');        
+          aboveMax && setInputError('Amount exceeds your current debt');
           break;
 
         case ActionCodes.ADD_COLLATERAL:
