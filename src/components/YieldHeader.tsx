9ad2a0f3f03ff3c5ad80222b283fcbf90b0c6af9
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-20 12:06:01 -0400

    update header to account for mobile (hamburger shows on mobile and opens up menulayer)

--- src/components/YieldHeader.tsx
+++ src/components/YieldHeader.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi';
 import { Box, Header, Grid, ResponsiveContext, Avatar } from 'grommet';
 
 import YieldNavigation from './YieldNavigation';
@@ -17,34 +18,38 @@ const YieldHeader = ({ actionList }: IYieldHeaderProps) => {
 
   return (
     <>
-      {!mobile && (
-        <Header
-          pad={mobile ? 'xsmall' : 'large'}
-          height={mobile ? undefined : 'xsmall'}
-          style={{ position: 'fixed', top: '0px' }}
-          direction="row"
-          fill="horizontal"
-        >
-          <Grid columns={['auto', '1fr', 'auto']} fill="horizontal">
-            <Box direction="row" gap={mobile ? '0.25em' : 'large'} align="center">
-              <Avatar background="hover" size="3rem" > 
-                <NavLink to={`/${prevLoc}`} style={{ height:'50%'}} >
+      <Header
+        pad={mobile ? 'medium' : 'large'}
+        height={mobile ? undefined : 'xsmall'}
+        style={{ position: 'fixed', top: '0px' }}
+        direction="row"
+        fill="horizontal"
+      >
+        <Grid columns={['auto', '1fr', 'auto']} fill="horizontal">
+          <Box direction="row" gap="large" align="center">
+            {mobile ? (
+              <Box onClick={actionList[0]}>
+                <FiMenu size="1.5rem" />
+              </Box>
+            ) : (
+              <Avatar background="hover" size="3rem">
+                <NavLink to={`/${prevLoc}`} style={{ height: '50%' }}>
                   <YieldMark
-                    height={mobile ? '1rem' : '1.75rem'}
+                    height="1.75rem"
                     colors={['#f79533', '#f37055', '#ef4e7b', '#a166ab', '#5073b8', '#1098ad', '#07b39b', '#6fba82']}
                   />
                 </NavLink>
               </Avatar>
-              {!mobile && <YieldNavigation />}
-            </Box>
-            <Box />
+            )}
+            {!mobile && <YieldNavigation />}
+          </Box>
+          <Box />
 
-            <Box align="end">
-              <YieldAccount />
-            </Box>
-          </Grid>
-        </Header>
-      )}
+          <Box align="end">
+            <YieldAccount />
+          </Box>
+        </Grid>
+      </Header>
     </>
   );
 };
