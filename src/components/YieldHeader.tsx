bf2e474b495c000087ac79862f1a8de600631d98
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-22 09:54:11 -0400

    incorporate back arrow instead of menu icon when in position

--- src/components/YieldHeader.tsx
+++ src/components/YieldHeader.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useRef } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory, useLocation } from 'react-router-dom';
 import { FiMenu } from 'react-icons/fi';
 import { Box, Header, Grid, ResponsiveContext, Avatar } from 'grommet';
 
@@ -8,13 +8,16 @@ import YieldAccount from './YieldAccount';
 import YieldMark from './logos/YieldMark';
 
 import { useCachedState } from '../hooks/generalHooks';
+import BackButton from './buttons/BackButton';
 
 interface IYieldHeaderProps {
   actionList: any[];
 }
 const YieldHeader = ({ actionList }: IYieldHeaderProps) => {
   const mobile: boolean = useContext<any>(ResponsiveContext) === 'small';
+  const history = useHistory();
   const prevLoc = useCachedState('lastVisit', '')[0].slice(1).split('/')[0];
+  const isPositionPath = useLocation().pathname.includes('position');
 
   return (
     <>
@@ -28,11 +31,13 @@ const YieldHeader = ({ actionList }: IYieldHeaderProps) => {
       >
         <Grid columns={['auto', '1fr', 'auto']} fill="horizontal">
           <Box direction="row" gap="large" align="center">
-            {mobile ? (
+            {mobile && !isPositionPath && (
               <Box onClick={actionList[0]}>
                 <FiMenu size="1.5rem" />
               </Box>
-            ) : (
+            )}
+            {mobile && isPositionPath && <BackButton action={() => history.goBack()} />}
+            {!mobile && (
               <Avatar background="hover" size="3rem">
                 <NavLink to={`/${prevLoc}`} style={{ height: '50%' }}>
                   <YieldMark
