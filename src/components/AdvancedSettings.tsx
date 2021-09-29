561d3560be63c27a95edd7016b2cf5d8f1053a00
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-29 11:47:30 -0400

    add approval setting Fixes #383

--- src/components/AdvancedSettings.tsx
+++ src/components/AdvancedSettings.tsx
@@ -2,27 +2,35 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Box, CheckBox, Text } from 'grommet';
 import { UserContext } from '../contexts/UserContext';
 import SlippageSettings from './SlippageSettings';
+import { ApprovalType } from '../types';
 
 const AdvancedSettings = () => {
   const {
-    userState: { showInactiveVaults },
-    userActions: { setShowInactiveVaults },
+    userState: { showInactiveVaults, approvalMethod },
+    userActions: { setShowInactiveVaults, setApprovalMethod },
   } = useContext(UserContext);
 
   return (
     <Box fill="horizontal" gap="medium">
-      <Box
-        direction="row"
-        justify="between"
-        pad={{ vertical: 'small' }}
-        border={{ color: 'tailwind-blue-100', side: 'bottom' }}
-      >
-        <Text size="small">Show Inactive Vaults</Text>
-        <CheckBox
-          toggle
-          checked={showInactiveVaults}
-          onChange={(event) => setShowInactiveVaults(event?.target.checked)}
-        />
+      <Box gap="small" pad={{ vertical: 'small' }} border={{ color: 'tailwind-blue-100', side: 'bottom' }}>
+        <Box direction="row" justify="between">
+          <Text size="small">Use Approval Method</Text>
+          <CheckBox
+            toggle
+            checked={approvalMethod === ApprovalType.TX}
+            onChange={(event) =>
+              event?.target.checked ? setApprovalMethod(ApprovalType.TX) : setApprovalMethod(ApprovalType.SIG)
+            }
+          />
+        </Box>
+        <Box direction="row" justify="between">
+          <Text size="small">Show Inactive Vaults</Text>
+          <CheckBox
+            toggle
+            checked={showInactiveVaults}
+            onChange={(event) => setShowInactiveVaults(event?.target.checked)}
+          />
+        </Box>
       </Box>
       <SlippageSettings />
     </Box>
