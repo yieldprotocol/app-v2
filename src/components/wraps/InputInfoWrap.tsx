aec63ca3c6f1579466645b5b7ae5f09febfc6809
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-28 22:03:50 -0400

    formatting

--- src/components/wraps/InputInfoWrap.tsx
+++ src/components/wraps/InputInfoWrap.tsx
@@ -6,19 +6,18 @@ import BoxWrap from './BoxWrap';
 
 function InputInfoWrap({ action, index, children }: { children: any; index?: number; action?: () => void }) {
   return (
-
-        <BoxWrap
-          pad='small'
-          direction="row"
-          gap="small"
-          align="center"
-          animation= { action? undefined : "zoomIn"}
-          onClick={action ? () => action() : undefined}
-          fill='horizontal'
-        >
-          {action?  <FiPlayCircle size="1.25rem" color="green" /> :   <FiInfo size="1.25rem" color="orange" />}
-          {children}
-        </BoxWrap>
+    <BoxWrap
+      pad="small"
+      direction="row"
+      gap="small"
+      align="center"
+      animation={action ? undefined : 'zoomIn'}
+      onClick={action ? () => action() : undefined}
+      fill="horizontal"
+    >
+      {action ? <FiPlayCircle size="1.25rem" color="green" /> : <FiInfo size="1.25rem" color="orange" />}
+      {children}
+    </BoxWrap>
   );
 }
 
