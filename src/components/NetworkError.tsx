56c242b8817caa7f0524fde11d90cce6104cea29
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-14 17:21:53 -0400

    fixing render

--- src/components/NetworkError.tsx
+++ src/components/NetworkError.tsx
@@ -13,14 +13,16 @@ const NetworkError = () => {
   return (
     <Layer>
       <Box pad="medium" round="small" gap="small" align="center" width="600px">
-        <FiAlertCircle size="2em" /> <Text size="large"> Oops. Unsupported network</Text>
+        <FiAlertCircle size="2em" /> <Text size="large">Oops. Unsupported network</Text>
         <Text size="small"> Please connect your wallet to one of the supported networks: </Text>
-        <Box direction="row" gap="xsmall">
-          {[...CHAIN_INFO.values()].map((c) => (
-            <Text color={c.color} size="small" key={c.name}>
-              {c.supported && c.name}
-            </Text>
-          ))}
+        <Box gap="xsmall">
+          {[...CHAIN_INFO.values()].map((c) =>
+            c.supported ? (
+              <Text color={c.color} size="small" key={c.name}>
+                {c.name}
+              </Text>
+            ) : null
+          )}
         </Box>
       </Box>
     </Layer>
