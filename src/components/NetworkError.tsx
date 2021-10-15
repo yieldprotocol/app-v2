fa5e4616f69c7cbda8ac6ea8aac10e09610c997e
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-15 15:23:10 -0400

    formatting

--- src/components/NetworkError.tsx
+++ src/components/NetworkError.tsx
@@ -6,27 +6,32 @@ import { ChainContext } from '../contexts/ChainContext';
 const NetworkError = () => {
   const {
     chainState: { connection },
-    chainActions: { disconnect }
+    chainActions: { disconnect },
   } = useContext(ChainContext);
 
-  const [ showError, setShowError] =  useState<boolean>(false);
+  const [showError, setShowError] = useState<boolean>(false);
 
-  useEffect(()=>{
-    connection.errorMessage ? setShowError(true) : setShowError(false); 
-  },[connection.errorMessage])
+  useEffect(() => {
+    connection.errorMessage ? setShowError(true) : setShowError(false);
+  }, [connection.errorMessage]);
 
   return (
     <>
-    { showError &&
-    <Layer>
-      <Box pad="medium" round="small" gap="small" align="center" width="600px">
-        <FiAlertCircle size="2em" /> <Text size="large">Oops. There was a connection error. </Text>
-        <Text size="small"> {connection.errorMessage} </Text>
-
-        <Button label='Continue on default network without connecting a wallet' onClick={()=> { setShowError(false); disconnect()}  }/> 
-      </Box>
-    </Layer>
-    }
+      {showError && (
+        <Layer>
+          <Box pad="medium" round="small" gap="small" align="center" width="600px">
+            <FiAlertCircle size="2em" /> <Text size="large">Oops. There was a connection error.</Text>
+            <Text size="small"> {connection.errorMessage} </Text>
+            <Button
+              label="Continue on default network without connecting a wallet"
+              onClick={() => {
+                setShowError(false);
+                disconnect();
+              }}
+            />
+          </Box>
+        </Layer>
+      )}
     </>
   );
 };
