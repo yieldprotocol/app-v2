543e47ad8670af31aef7472e689c444139c7c7d7
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-22 11:46:18 -0400

    adding connect modal

--- src/components/wraps/ActionButtonWrap.tsx
+++ src/components/wraps/ActionButtonWrap.tsx
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Box, Text, Layer, ResponsiveContext } from 'grommet';
 import { ChainContext } from '../../contexts/ChainContext';
+import Connect from '../Connect';
+import SidebarSettings from '../SidebarSettings';
 
 function ActionButtonWrap({ children, pad }: { children: any; pad?: boolean }) {
   const mobile: boolean = useContext<any>(ResponsiveContext) === 'small';
@@ -12,6 +14,8 @@ function ActionButtonWrap({ children, pad }: { children: any; pad?: boolean }) {
     chainActions: { connect },
   } = useContext(ChainContext);
 
+  const [connectOpen, setConnectOpen] = useState<boolean>(false);
+
   return mobile ? (
     <Layer position="bottom" background="white" modal={false} responsive={false} full="horizontal" animate={false}>
       <Box gap="small" fill="horizontal" pad="small">
@@ -19,21 +23,24 @@ function ActionButtonWrap({ children, pad }: { children: any; pad?: boolean }) {
       </Box>
     </Layer>
   ) : (
-    <Box
-      gap="small"
-      fill="horizontal"
-      pad={pad ? { horizontal: 'large', vertical: 'medium', bottom: 'large' } : undefined}
-    >
-      {account ? (
-        children
-      ) : (
-        <Button
-          secondary
-          label={<Text size={mobile ? 'small' : undefined}> Connect Wallet </Text>}
-          onClick={() => connect()}
-        />
-      )}
-    </Box>
+    <>
+      <Box
+        gap="small"
+        fill="horizontal"
+        pad={pad ? { horizontal: 'large', vertical: 'medium', bottom: 'large' } : undefined}
+      >
+        {account ? (
+          children
+        ) : (
+          <Button
+            secondary
+            label={<Text size={mobile ? 'small' : undefined}>Connect Wallet</Text>}
+            onClick={() => setConnectOpen(true)}
+          />
+        )}
+      </Box>
+      {connectOpen && <SidebarSettings connectOpen={connectOpen} setConnectOpen={setConnectOpen} />}
+    </>
   );
 }
 
