274b45495bf4d8a89463287dd32d94e6e1cc4564
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-02 13:14:18 -0400

    update shadow to handle dark mode

--- src/components/texts/ColorText.tsx
+++ src/components/texts/ColorText.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text } from 'grommet';
-import styled from 'styled-components';
+import styled, { ThemeContext } from 'styled-components';
 
 const StyledText = styled(Text)`
   font-family: 'Raleway';
   background: -webkit-linear-gradient(95deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82);
-  -webkit-background-clip: text; 
+  -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  filter: drop-shadow(2px 2px 2px #ddd);  */
+  filter: ${(props) => (props.theme.dark ? 'drop-shadow(2px 2px 2px #141a1e)}' : 'drop-shadow(2px 2px 2px #ddd)')};
 `;
 
-const ColorText = (props: any) => <StyledText {...props} selected={props.selected}>{props.children}</StyledText>;
+const ColorText = (props: any) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <StyledText {...props} selected={props.selected} theme={theme}>
+      {props.children}
+    </StyledText>
+  );
+};
 
 export default ColorText;
