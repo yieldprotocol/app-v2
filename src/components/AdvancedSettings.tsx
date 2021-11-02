05e5077a399dca4d76dbbc50d72a334cfa2600fb
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-01 20:02:55 -0400

    add darkMode toggle

--- src/components/AdvancedSettings.tsx
+++ src/components/AdvancedSettings.tsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import { Box, CheckBox, Text } from 'grommet';
+import { FiMoon, FiSun } from 'react-icons/fi';
+import { ThemeContext } from 'styled-components';
+import Switch from 'react-switch';
 import { UserContext } from '../contexts/UserContext';
 import SlippageSettings from './SlippageSettings';
 import { ApprovalType } from '../types';
@@ -8,9 +11,12 @@ import { ChainContext } from '../contexts/ChainContext';
 
 const AdvancedSettings = () => {
   const {
-    userState: { approvalMethod },
-    userActions: { setApprovalMethod },
+    userState: { approvalMethod, darkMode },
+    userActions: { setApprovalMethod, toggleDarkMode },
   } = useContext(UserContext);
+
+  const theme = useContext<any>(ThemeContext);
+  const { purple } = theme.global.colors;
 
   const {
     chainState: {
@@ -19,12 +25,18 @@ const AdvancedSettings = () => {
   } = useContext(ChainContext);
 
   const [, setCachedApprovalMethod] = useCachedState('cachedApprovalMethod', approvalMethod);
+  const [, toggleCachedDarkMode] = useCachedState('darkMode', darkMode);
 
   const handleApprovalToggle = (type: ApprovalType) => {
     /* set for current session */
     setApprovalMethod(type);
     /* set cached for future sessions */
     setCachedApprovalMethod(type);
+  };
+
+  const handleDarkModeToggle = (isDark: boolean) => {
+    toggleDarkMode(isDark);
+    toggleCachedDarkMode(isDark);
   };
 
   if (connectionName === 'ledgerWithMetamask') return null;
@@ -39,6 +51,28 @@ const AdvancedSettings = () => {
             onChange={(event) =>
               event?.target.checked ? handleApprovalToggle(ApprovalType.TX) : handleApprovalToggle(ApprovalType.SIG)
             }
+          />
+        </Box>
+        <Box direction="row" justify="between">
+          <Text size="small">Theme</Text>
+          <Switch
+            width={55}
+            checked={darkMode}
+            uncheckedIcon={
+              <Box align="center" justify="center" fill pad="xsmall">
+                <FiSun color="gray" />
+              </Box>
+            }
+            checkedIcon={
+              <Box align="center" justify="center" fill pad="xsmall">
+                <FiMoon color={purple} />
+              </Box>
+            }
+            offColor="#EFF6FF"
+            onColor="#BFDBFE"
+            onChange={handleDarkModeToggle}
+            handleDiameter={20}
+            borderRadius={20}
           />
         </Box>
       </Box>
