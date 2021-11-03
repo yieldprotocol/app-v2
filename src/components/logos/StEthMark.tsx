cba9a891e2a02e2dcf04a206a8ac2e502a3e6077
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-03 09:25:37 -0400

    update viewbox

--- src/components/logos/StEthMark.tsx
+++ src/components/logos/StEthMark.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function StEthMark(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg width="14" height="20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg width="14" height="20" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 20" {...props}>
       <path
         d="M7.007 0l4.95 7.748-4.95 2.884-4.95-2.884L7.007 0zM3.572 7.381l3.435-5.376 3.434 5.376-3.435 2.002L3.572 7.38z"
         fill="#00A3FF"
