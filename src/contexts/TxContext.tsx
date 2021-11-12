3d5917bc48b2764c2dfa9981077a69d8b0bf2ffa
brucedonovan <bruce.donovan@gmail.com>
2021-11-12 09:33:31 +0000

    remove sigData  req. on handleSign

--- src/contexts/TxContext.tsx
+++ src/contexts/TxContext.tsx
@@ -207,7 +207,6 @@ const TxProvider = ({ children }: any) => {
   const handleSign = async (
     signFn: () => Promise<any>,
     fallbackFn: () => Promise<any>,
-    sigData: ISignData,
     txCode: string,
     approvalMethod: ApprovalType
   ) => {
