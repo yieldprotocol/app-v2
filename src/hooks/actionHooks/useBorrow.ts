c98f82f8ddabdd9480b8b2b754355d29784908b2
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-17 14:18:35 -0500

    update selected Ilk as well after borrow

--- src/hooks/actionHooks/useBorrow.ts
+++ src/hooks/actionHooks/useBorrow.ts
@@ -2,7 +2,17 @@ import { ethers } from 'ethers';
 import { useContext } from 'react';
 import { SettingsContext } from '../../contexts/SettingsContext';
 import { UserContext } from '../../contexts/UserContext';
-import { ICallData, IVault, ActionCodes, LadleActions, ISeries, IAsset, IUserContext, IUserContextActions, IUserContextState } from '../../types';
+import {
+  ICallData,
+  IVault,
+  ActionCodes,
+  LadleActions,
+  ISeries,
+  IAsset,
+  IUserContext,
+  IUserContextActions,
+  IUserContextState,
+} from '../../types';
 import { cleanValue, getTxCode } from '../../utils/appUtils';
 import { ETH_BASED_ASSETS, BLANK_VAULT, MAX_256 } from '../../utils/constants';
 import { buyBase, calculateSlippage } from '../../utils/yieldMath';
@@ -15,9 +25,9 @@ export const useBorrow = () => {
     settingsState: { slippageTolerance, approveMax },
   } = useContext(SettingsContext);
 
-    const { userState, userActions }: { userState: IUserContextState; userActions: IUserContextActions } = useContext(
+  const { userState, userActions }: { userState: IUserContextState; userActions: IUserContextActions } = useContext(
     UserContext
-  ) as IUserContext;;
+  ) as IUserContext;
   const { activeAccount: account, selectedIlk, selectedSeries, seriesMap, assetMap } = userState;
   const { updateVaults, updateAssets, updateSeries } = userActions;
 
@@ -33,7 +43,7 @@ export const useBorrow = () => {
 
     /* Set the series and ilk based on the vault that has been selected or if it's a new vault, get from the globally selected SeriesId */
     const series: ISeries = vault ? seriesMap.get(vault.seriesId)! : selectedSeries!;
-    const base : IAsset = assetMap.get(series.baseId)!;
+    const base: IAsset = assetMap.get(series.baseId)!;
     const ilk: IAsset = vault ? assetMap.get(vault.ilkId)! : assetMap.get(selectedIlk?.idToUse!)!; // note: we use the wrapped version if required
 
     /* parse inputs  ( clean down to base/ilk decimals so that there is never an underlow)  */
@@ -53,9 +63,7 @@ export const useBorrow = () => {
     const _expectedFyTokenWithSlippage = calculateSlippage(_expectedFyToken, slippageTolerance);
 
     /* if approveMAx, check if signature is required */
-    const alreadyApproved = approveMax
-      ? (await ilk.getAllowance(account!, ilk.joinAddress)).gt(_collInput)
-      : false;
+    const alreadyApproved = approveMax ? (await ilk.getAllowance(account!, ilk.joinAddress)).gt(_collInput) : false;
 
     const wrapping: ICallData[] = await wrapAssetToJoin(_collInput, selectedIlk!, txCode); // note: selected ilk used here, not wrapped version
 
@@ -77,7 +85,6 @@ export const useBorrow = () => {
 
     /* Collate all the calls required for the process (including depositing ETH, signing permits, and building vault if needed) */
     const calls: ICallData[] = [
-
       /* handle wrapped token deposit, if required */
       ...wrapping,
 
@@ -108,7 +115,7 @@ export const useBorrow = () => {
       else update ALL vaults (by passing an empty array)
     */
     updateVaults([]);
-    updateAssets([base, ilk]);
+    updateAssets([base, ilk, selectedIlk!]);
     updateSeries([series]);
   };
 
