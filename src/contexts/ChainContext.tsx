f3d63f010d2723912bb1384df873d6b0d1f28106
brucedonovan <bruce.donovan@gmail.com>
2021-11-30 10:14:08 +0000

    gracefully handle new additions

--- src/contexts/ChainContext.tsx
+++ src/contexts/ChainContext.tsx
@@ -254,25 +254,16 @@ const ChainProvider = ({ children }: any) => {
               ERC20.symbol(),
               ERC20.decimals(),
               id === USDC ? '2' : '1', // TODO  ERC20.version()
-            ]);
+            ]); 
 
-            const {
-              showToken,
-              isWrappedToken,
-              wrapHandlerAddress,
-              wrappedTokenId,
-              wrappedTokenAddress,
-              displaySymbol,
-              unwrappedTokenId,
-            } = ASSET_INFO.get(symbol) as IAssetInfo;  
-
-            const idToUse = wrappedTokenId || id;
+            const assetInfo = ASSET_INFO.get(symbol) as IAssetInfo;
+            const idToUse = assetInfo?.wrappedTokenId || id;
 
             const newAsset = {
               id,
               address,
               name,
-              displaySymbol: displaySymbol || symbol,
+              
               symbol,
               decimals,
               version,
@@ -280,13 +271,14 @@ const ChainProvider = ({ children }: any) => {
               joinAddress: joinMap.get(idToUse),
               idToUse,
 
-              isWrappedToken,
-              wrapHandlerAddress,
-              wrappedTokenId,
-              wrappedTokenAddress,
-              unwrappedTokenId,
+              displaySymbol: assetInfo?.displaySymbol || symbol,
 
-              showToken,
+              isWrappedToken: assetInfo?.isWrappedToken,
+              wrapHandlerAddress: assetInfo?.wrapHandlerAddress,
+              wrappedTokenId: assetInfo?.wrappedTokenId,
+              wrappedTokenAddress: assetInfo?.wrappedTokenAddress,
+              unwrappedTokenId: assetInfo?.unwrappedTokenId,
+              showToken: assetInfo?.showToken || false,
             };
 
             // Update state and cache
