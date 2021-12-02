bf74d9a3b00a4fc92473943f7a994ed76910b81d
marcomariscal <marco.a.mariscal@gmail.com>
2021-12-02 10:49:13 -0500

    fix typing

--- src/config/oracles.ts
+++ src/config/oracles.ts
@@ -5,29 +5,17 @@ const CHAINLINK_MULTI_ORACLE = 'CHAINLINK_MULTI_ORACLE';
 const CHAINLINK_USD_ORACLE = 'CHAINLINK_USD_ORACLE';
 const ACCUMULATOR_MULTI_ORACLE = 'AccumulatorMultiOracle';
 
-interface IOracleInfo {
-  [chainId: number]: IAssetPairOracleInfo;
-}
-
-interface IAssetPairOracleInfo {
-  [baseId: string]: Map<IIlkToOracle>;
-}
-
-interface IIlkToOracle {
-  [ilkId: string]: string;
-}
-
 // map chain id to oracle info
-export const ORACLE_INFO = new Map<IOracleInfo>();
+export const ORACLE_INFO = new Map<number, Map<string, Map<string, string>>>();
 
 // map asset (baseId) and other asset (ilkId) to a specific oracle based on where there is relevant price info for the pair
-export const CHAIN_ID_1_ASSET_ORACLE_INFO = new Map<IAssetPairOracleInfo>();
-export const CHAIN_ID_42_ASSET_ORACLE_INFO = new Map<IAssetPairOracleInfo>();
-export const CHAIN_ID_421611_ASSET_ORACLE_INFO = new Map<IAssetPairOracleInfo>();
+export const CHAIN_ID_1_ASSET_ORACLE_INFO = new Map<string, Map<string, string>>();
+export const CHAIN_ID_42_ASSET_ORACLE_INFO = new Map<string, Map<string, string>>();
+export const CHAIN_ID_421611_ASSET_ORACLE_INFO = new Map<string, Map<string, string>>();
 
 /* chain id 1, 42 */
 // USDC base
-const usdcIlkOracle1 = new Map<IIlkToOracle>();
+const usdcIlkOracle1 = new Map<string, string>();
 usdcIlkOracle1.set(WETH, CHAINLINK_MULTI_ORACLE);
 usdcIlkOracle1.set(DAI, CHAINLINK_MULTI_ORACLE);
 usdcIlkOracle1.set(USDC, CHAINLINK_MULTI_ORACLE);
@@ -40,7 +28,7 @@ usdcIlkOracle1.set(UNI, CHAINLINK_MULTI_ORACLE);
 CHAIN_ID_1_ASSET_ORACLE_INFO.set(USDC, usdcIlkOracle1);
 
 // DAI base
-const daiIlkOracle1 = new Map<IIlkToOracle>();
+const daiIlkOracle1 = new Map<string, string>();
 daiIlkOracle1.set(WETH, CHAINLINK_MULTI_ORACLE);
 daiIlkOracle1.set(DAI, CHAINLINK_MULTI_ORACLE);
 daiIlkOracle1.set(USDC, CHAINLINK_MULTI_ORACLE);
@@ -54,7 +42,7 @@ CHAIN_ID_1_ASSET_ORACLE_INFO.set(DAI, daiIlkOracle1);
 
 /* chain id 421611 */
 // USDC base
-const usdcIlkOracle421611 = new Map<IIlkToOracle>();
+const usdcIlkOracle421611 = new Map<string, string>();
 usdcIlkOracle421611.set(WETH, CHAINLINK_USD_ORACLE);
 usdcIlkOracle421611.set(DAI, CHAINLINK_USD_ORACLE);
 usdcIlkOracle421611.set(USDC, CHAINLINK_USD_ORACLE);
@@ -67,7 +55,7 @@ usdcIlkOracle421611.set(UNI, CHAINLINK_USD_ORACLE);
 CHAIN_ID_421611_ASSET_ORACLE_INFO.set(USDC, usdcIlkOracle421611);
 
 // DAI base
-const daiIlkOracle421611 = new Map<IIlkToOracle>();
+const daiIlkOracle421611 = new Map<string, string>();
 daiIlkOracle421611.set(WETH, CHAINLINK_USD_ORACLE);
 daiIlkOracle421611.set(DAI, CHAINLINK_USD_ORACLE);
 daiIlkOracle421611.set(USDC, CHAINLINK_USD_ORACLE);
