81c265f641736a6f5a8861775b26cb61b040d08a
marcomariscal <marco.a.mariscal@gmail.com>
2021-12-14 13:24:50 -0800

    remove console log

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -640,7 +640,6 @@ const UserProvider = ({ children }: any) => {
   /* When the chainContext is finished loading get the users vault data */
   useEffect(() => {
     if (!chainLoading && account) {
-      console.log('account', account);
       console.log('Checking User Vaults');
       /* trigger update of update all vaults by passing empty array */
       updateVaults([]);
