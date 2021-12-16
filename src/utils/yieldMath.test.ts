816062e43c9cb3ff3054579e0de95b101ffd5280
marcomariscal <marco.a.mariscal@gmail.com>
2021-12-16 12:31:37 -0800

    add mocha

---
+++ src/utils/yieldMath.test.ts
@@ -0,0 +1,11 @@
+import { beforeEach, describe } from 'mocha';
+import { expect } from 'chai';
+
+describe('VY YieldMath', () => {
+  beforeEach(() => {});
+  describe('sellBaseVY (fyDaiOutForVYDaiIn)', () => {
+    it('should be positive trade', () => {
+      assert.ok(true);
+    });
+  });
+});
