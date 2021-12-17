7952b4a03eb9ee0f717d2108a43cb954f5f2211e
marcomariscal <marco.a.mariscal@gmail.com>
2021-12-16 16:38:39 -0800

    testing inputs into variable interest base yieldmath funcs

--- src/test/yieldMath.test.ts
+++ src/test/yieldMath.test.ts
@@ -1,14 +1,37 @@
-import { expect } from 'chai';
-import { BigNumber } from 'ethers';
+import chai, { expect } from 'chai';
+import { solidity } from 'ethereum-waffle';
+import { BigNumber, utils } from 'ethers';
+import { sellBase, sellBaseVY } from '../utils/yieldMath';
+
+chai.use(solidity);
+const { parseUnits } = utils;
 
 describe('VY YieldMath', () => {
-  let c: BigNumber;
+  let baseReserves: BigNumber | string; // z
+  let fyTokenReserves: BigNumber | string; // y
+  let base: BigNumber | string;
+  let coefficient: BigNumber | string; // c: the price of vyDAI to DAI
+  let timeTillMaturity: BigNumber | string;
+
   beforeEach(() => {
-    c = BigNumber.from('1');
+    baseReserves = parseUnits('1', 27); // z
+    fyTokenReserves = parseUnits('2', 27); // y
+    base = parseUnits('1.84', 24);
+    coefficient = '1'; // c: the price of vyDAI to DAI
+    timeTillMaturity = parseUnits('1', 7);
   });
+
   describe('sellBaseVY (fyDaiOutForVYDaiIn)', () => {
-    it('should ', () => {
-      expect(1).to.equal(1);
+    // https://www.desmos.com/calculator/wh5quy9mft
+    it('should equal non-variable yield func with non-variable base', () => {
+      const resultVY = sellBaseVY(baseReserves, fyTokenReserves, base, coefficient, timeTillMaturity, 18);
+      const result = sellBase(baseReserves, fyTokenReserves, base, timeTillMaturity, 18);
+      expect(resultVY).to.equal(result);
+    });
+
+    it('should equal some number with certain inputs', () => {
+      const result = sellBaseVY(baseReserves, fyTokenReserves, base, coefficient, timeTillMaturity, 18);
+      expect(result).to.be.within(BigNumber.from(parseUnits('1.8', 24)), BigNumber.from(parseUnits('2', 24)));
     });
   });
 });
