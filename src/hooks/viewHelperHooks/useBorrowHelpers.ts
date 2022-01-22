3207741e340befe9d3d18b7a0c038b59f2c0a8a0
brucedonovan <bruce.donovan@gmail.com>
2022-01-22 15:42:08 +0000

    temp disable setting maxbaseIn repay limit.

--- src/hooks/viewHelperHooks/useBorrowHelpers.ts
+++ src/hooks/viewHelperHooks/useBorrowHelpers.ts
@@ -190,15 +190,17 @@ export const useBorrowHelpers = (
 
         /* if maxBasein is less than debt, and set protocol Limited flag */
         if (_maxBaseIn.lt(vault.accruedArt) && !vaultSeries.seriesIsMature) {
-          setProtocolLimited(true);
+          setProtocolLimited(false);
         } else {
           setProtocolLimited(false);
         }
 
         /* set the maxRepay as the biggest of the two, human readbale and BN */
         if (_maxRepayable && _maxBaseIn && _maxRepayable.gt(_maxBaseIn)) {
-          setMaxRepay_(ethers.utils.formatUnits(_maxBaseIn, vaultBase?.decimals)?.toString());
-          setMaxRepay(_maxBaseIn);
+          setMaxRepay_(ethers.utils.formatUnits(_maxRepayable, vaultBase?.decimals)?.toString());
+          setMaxRepay(_maxRepayable);
+          // setMaxRepay_(ethers.utils.formatUnits(_maxBaseIn, vaultBase?.decimals)?.toString());
+          // setMaxRepay(_maxBaseIn);
         } else {
           setMaxRepay_(ethers.utils.formatUnits(_maxRepayable, vaultBase?.decimals)?.toString());
           setMaxRepay(_maxRepayable);
