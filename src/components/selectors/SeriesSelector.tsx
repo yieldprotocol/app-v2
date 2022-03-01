5ff082fa3adb4fb1cc13c79048411ba677b414a2
brucedonovan <bruce.donovan@gmail.com>
2022-03-01 11:40:09 +0000

    verbiage series selector

--- src/components/selectors/SeriesSelector.tsx
+++ src/components/selectors/SeriesSelector.tsx
@@ -132,7 +132,7 @@ function SeriesSelector({ selectSeriesLocally, inputValue, actionType, cardLayou
     if (_series) {
       return `${mobile ? _series.displayNameMobile : _series.displayName}`;
     }
-    return 'Select a future maturity';
+    return 'Select a maturity';
   };
 
   const optionExtended = (_series: ISeries | undefined) => (
