26131f98007ba37e909ba645744a505cc846c76d
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-10 17:04:18 -0500

    fix: fill modal

--- src/components/selectors/StrategySelector.tsx
+++ src/components/selectors/StrategySelector.tsx
@@ -166,7 +166,7 @@ function StrategySelector({ inputValue, cardLayout, setOpen, open = false }: ISt
                     onClickOutside={() => setOpen(false)}
                     style={{ minWidth: mobile ? undefined : '500px', borderRadius: '12px' }}
                   >
-                    <Box gap="small" pad="medium" round="small" background="background">
+                    <Box gap="small" pad="medium" round="small" background="background" fill>
                       <Box alignSelf="end" onClick={() => setOpen(false)}>
                         <FiX size="1.5rem" />
                       </Box>
