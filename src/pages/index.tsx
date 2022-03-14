b67098fd5e23e795ef2112fbc6dbd369f322cac0
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-14 12:24:20 -0400

    remove react items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
 import { ethers } from 'ethers';
 import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
 
