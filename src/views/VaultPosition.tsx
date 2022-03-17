f44a754b959665efe3f99051cf4e7706b65f2809
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-17 16:28:20 -0400

    default to true for reclaiming collateral when repaying all debt

--- src/views/VaultPosition.tsx
+++ src/views/VaultPosition.tsx
@@ -94,7 +94,7 @@ const VaultPosition = () => {
   const [stepPosition, setStepPosition] = useState<number[]>(new Array(7).fill(0));
 
   const [repayInput, setRepayInput] = useState<any>(undefined);
-  const [reclaimCollateral, setReclaimCollateral] = useState<boolean>(false);
+  const [reclaimCollateral, setReclaimCollateral] = useState<boolean>(true);
 
   const [addCollatInput, setAddCollatInput] = useState<any>(undefined);
   const [removeCollatInput, setRemoveCollatInput] = useState<any>(undefined);
