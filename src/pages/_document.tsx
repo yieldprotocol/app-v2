be21cac4c91fed67becf319da144b7a237a83716
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-01 14:40:15 -0400

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;0,900;1,100;1,400;1,700;1,900&display=optional"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
         </Head>
         <body>
           <script
