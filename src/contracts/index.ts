8cb89a5eb3a95e3c563b9397a476a0658ff25d02
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-06 14:57:27 -0400

    update

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -8,7 +8,6 @@ export type { ChainlinkUSDOracle } from "./ChainlinkUSDOracle";
 export type { CompositeMultiOracle } from "./CompositeMultiOracle";
 export type { CompoundMultiOracle } from "./CompoundMultiOracle";
 export type { ConvexLadleModule } from "./ConvexLadleModule";
-export type { CVX3CRVOracle } from "./CVX3CRVOracle";
 export type { ERC1155 } from "./ERC1155";
 export type { ERC20 } from "./ERC20";
 export type { ERC20Permit } from "./ERC20Permit";
@@ -34,7 +33,6 @@ export { ChainlinkUSDOracle__factory } from "./factories/ChainlinkUSDOracle__fac
 export { CompositeMultiOracle__factory } from "./factories/CompositeMultiOracle__factory";
 export { CompoundMultiOracle__factory } from "./factories/CompoundMultiOracle__factory";
 export { ConvexLadleModule__factory } from "./factories/ConvexLadleModule__factory";
-export { CVX3CRVOracle__factory } from "./factories/CVX3CRVOracle__factory";
 export { ERC1155__factory } from "./factories/ERC1155__factory";
 export { ERC20__factory } from "./factories/ERC20__factory";
 export { ERC20Permit__factory } from "./factories/ERC20Permit__factory";
