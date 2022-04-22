fdb5582ef7859fd91c5c4a47f8be7b2bf044ad42
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-22 12:33:41 -0400

    add backup alchemy mainnet instance key

--- next.config.js
+++ next.config.js
@@ -13,6 +13,9 @@ const nextConfig = {
     REACT_APP_RPC_URL_69: process.env.REACT_APP_RPC_URL_69,
     REACT_APP_RPC_URL_42161: process.env.REACT_APP_RPC_URL_42161,
     REACT_APP_RPC_URL_421611: process.env.REACT_APP_RPC_URL_421611,
+    INFURA_KEY: process.env.INFURA_KEY,
+    ALCHEMY_ARBITRUM_KEY: process.env.ALCHEMY_ARBITRUM_KEY,
+    ALCHEMY_ARBITRUM_RINKEBY_KEY: process.env.ALCHEMY_ARBITRUM_RINKEBY_KEY,
   },
   async redirects() {
     return [
