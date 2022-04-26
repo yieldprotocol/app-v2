21f62a6409c80cc92656ae2a5acbb70dc9e2161c
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-26 15:36:36 -0400

    refactor to use button and show disabled when no balance

--- src/components/selectors/AssetSelectModal.tsx
+++ src/components/selectors/AssetSelectModal.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Layer, Text, ThemeContext } from 'grommet';
+import { ethers } from 'ethers';
+import { Avatar, Box, Button, Layer, Text, ThemeContext } from 'grommet';
 import { useContext } from 'react';
 import { FiX } from 'react-icons/fi';
 import styled from 'styled-components';
@@ -36,34 +37,32 @@ const AssetSelectModal = ({ assets, handleSelect, open, setOpen }: IAssetSelectM
         <Line color={theme === 'dark' ? gradient.dark : gradient.light} />
 
         <Box width="medium">
-          <Box overflow="auto" height="500px">
+          <Box overflow="auto" height={{ max: '500px' }}>
             {assets.map((a) => (
-              <Box
+              <Button
                 key={a.id}
-                justify="between"
-                direction="row"
-                align="center"
-                pad={{ horizontal: 'medium', vertical: 'xsmall' }}
-                hoverIndicator={theme === 'dark' ? 'hoverBackground' : 'background'}
                 onClick={() => {
                   handleSelect(a);
                   setOpen(false);
                 }}
-                fill
+                hoverIndicator={theme === 'dark' ? 'hoverBackground' : 'background'}
+                disabled={a.balance.eq(ethers.constants.Zero)}
               >
-                <Box direction="row" gap="small" align="center">
-                  <Avatar size="xsmall" background={theme === 'dark' ? 'text' : undefined}>
-                    {a.image}
-                  </Avatar>
-                  <Box>
-                    <Text size="small">{a.displaySymbol}</Text>
-                    <Text size="xsmall" color="text-weak">
-                      {a.name}
-                    </Text>
+                <Box justify="between" direction="row" align="center" pad={{ horizontal: 'medium', vertical: 'small' }}>
+                  <Box direction="row" gap="small" align="center">
+                    <Avatar size="xsmall" background={theme === 'dark' ? 'text' : undefined}>
+                      {a.image}
+                    </Avatar>
+                    <Box>
+                      <Text size="small">{a.displaySymbol}</Text>
+                      <Text size="xsmall" color="text-weak">
+                        {a.name}
+                      </Text>
+                    </Box>
                   </Box>
+                  <Text size="small">{a.balance_}</Text>
                 </Box>
-                <Text size="small">{a.balance_}</Text>
-              </Box>
+              </Button>
             ))}
           </Box>
         </Box>
