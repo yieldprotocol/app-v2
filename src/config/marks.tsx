7cb6d602cae78b0c311b965b32c37d2537c9e2ca
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-27 07:36:35 -0400

    remove unnecessary

--- src/config/marks.tsx
+++ src/config/marks.tsx
@@ -12,25 +12,24 @@ import USDCMark from '../components/logos/USDCMark';
 import USDTMark from '../components/logos/USDTMark';
 import WBTCMark from '../components/logos/WBTCMark';
 import YFIMark from '../components/logos/YFIMark';
-import { ASSET_INFO, yvUSDC } from './assets';
 
 const markMap = new Map([
-  ['DAI', <DaiMark key="dai" height={24} width={24} />],
-  ['USDC', <USDCMark key="usdc" height={24} width={24} />],
-  ['WBTC', <WBTCMark key="wbtc" height={24} width={24} />],
-  ['TST', <TSTMark key="tst" height={24} width={24} />],
-  ['ETH', <EthMark key="eth" height={24} width={24} />],
-  ['USDT', <USDTMark key="usdt" height={24} width={24} />],
-  ['LINK', <LinkMark key="link" height={24} width={24} />],
-  ['wstETH', <StEthMark key="wsteth" height={24} width={24} />],
-  ['stETH', <StEthMark key="steth" height={24} width={24} />],
-  ['ENS', <ENSMark key="ens" height={24} width={24} />],
-  ['UNI', <UNIMark key="uni" height={24} width={24} />],
-  ['yvUSDC', <YFIMark key="yvusdc" color={ASSET_INFO?.get(yvUSDC)!.color} height={24} width={24} />],
-  ['MKR', <MakerMark key="mkr" height={24} width={24} />],
-  ['Notional', <NotionalMark color={ASSET_INFO?.get(yvUSDC)!.color} key="notional" height={24} width={24} />],
-  // ['Cvx3Crv Mock', <TriCRVCVXMark key="cvx3crv" height={24} width={24} />],
-  ['FRAX', <FRAXMark key="frax" height={24} width={24} />],
+  ['DAI', <DaiMark key="dai" />],
+  ['USDC', <USDCMark key="usdc" />],
+  ['WBTC', <WBTCMark key="wbtc" />],
+  ['TST', <TSTMark key="tst" />],
+  ['ETH', <EthMark key="eth" />],
+  ['USDT', <USDTMark key="usdt" />],
+  ['LINK', <LinkMark key="link" />],
+  ['wstETH', <StEthMark key="wsteth" />],
+  ['stETH', <StEthMark key="steth" />],
+  ['ENS', <ENSMark key="ens" />],
+  ['UNI', <UNIMark key="uni" />],
+  ['yvUSDC', <YFIMark key="yvusdc" />],
+  ['MKR', <MakerMark key="mkr" />],
+  ['Notional', <NotionalMark key="notional" />],
+  // ['Cvx3Crv Mock', <TriCRVCVXMark key="cvx3crv" />],
+  ['FRAX', <FRAXMark key="frax" />],
 ]);
 
 export default markMap;
