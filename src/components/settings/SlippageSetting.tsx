25b3c11138540b95d13f20aac83b9a78a5843942
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-28 07:46:31 -0400

    format

--- src/components/settings/SlippageSetting.tsx
+++ src/components/settings/SlippageSetting.tsx
@@ -27,7 +27,7 @@ const SlippageSetting = () => {
   /* Sets the slippage tolerance on input */
   useEffect(() => {
     const _slippageTolerance = validateInput(Number(cleanValue(input, 4)) / 100);
-    updateSetting( Settings.SLIPPAGE_TOLERANCE, _slippageTolerance);
+    updateSetting(Settings.SLIPPAGE_TOLERANCE, _slippageTolerance);
   }, [input]); // purpose ignore updateSetting
 
   /* handle slection - clear input on slection of preset */
