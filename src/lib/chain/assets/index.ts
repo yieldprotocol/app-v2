f52f23494e3da252f408bb0160d287d873ab8344
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-28 07:22:37 -0400

    add get assets func from chain context to its own file, and make specific to ssr

---
+++ src/lib/chain/assets/index.ts
@@ -0,0 +1,91 @@
+import { Contract, ethers } from 'ethers';
+import { ASSET_INFO, UNKNOWN } from '../../../config/assets';
+import { ERC20Permit__factory, ERC20__factory } from '../../../contracts';
+import { AssetAddedEvent } from '../../../contracts/Cauldron';
+import { JoinAddedEvent } from '../../../contracts/ConvexLadleModule';
+import { IAssetRoot, TokenType } from '../../../types';
+
+export const getAssets = async (
+  provider: ethers.providers.JsonRpcProvider | ethers.providers.Web3Provider,
+  contractMap: Map<string, Contract>
+) => {
+  const Cauldron = contractMap.get('Cauldron');
+  const Ladle = contractMap.get('Ladle');
+
+  const [assetAddedEvents, joinAddedEvents] = await Promise.all([
+    Cauldron.queryFilter('AssetAdded' as ethers.EventFilter),
+    Ladle.queryFilter('JoinAdded' as ethers.EventFilter),
+  ]);
+
+  /* Create a map from the joinAdded event data or hardcoded join data if available */
+  const joinMap = new Map(joinAddedEvents.map((e: JoinAddedEvent) => e.args)); // event values);
+
+  /* Create a array from the assetAdded event data or hardcoded asset data if available */
+  const assetsAdded = assetAddedEvents.map((e: AssetAddedEvent) => e.args);
+
+  return assetsAdded.reduce(async (assetMap, x) => {
+    const { assetId: id, asset: address } = x;
+
+    /* Get the basic hardcoded token info */
+    const assetInfo = ASSET_INFO.has(id) ? ASSET_INFO.get(id) : ASSET_INFO.get(UNKNOWN);
+    let { name, symbol, decimals, version } = assetInfo;
+
+    /* On first load Checks/Corrects the ERC20 name/symbol/decimals  (if possible ) */
+    if (
+      assetInfo.tokenType === TokenType.ERC20_ ||
+      assetInfo.tokenType === TokenType.ERC20_Permit ||
+      assetInfo.tokenType === TokenType.ERC20_DaiPermit
+    ) {
+      const contract = ERC20__factory.connect(address, provider);
+      try {
+        [name, symbol, decimals] = await Promise.all([contract.name(), contract.symbol(), contract.decimals()]);
+      } catch (e) {
+        console.log(
+          address,
+          ': ERC20 contract auto-validation unsuccessfull. Please manually ensure symbol and decimals are correct.'
+        );
+      }
+    }
+
+    /* Checks/Corrects the version for ERC20Permit tokens */
+    if (assetInfo.tokenType === TokenType.ERC20_Permit || assetInfo.tokenType === TokenType.ERC20_DaiPermit) {
+      const contract = ERC20Permit__factory.connect(address, provider);
+      try {
+        version = await contract.version();
+      } catch (e) {
+        console.log(
+          address,
+          ': contract version auto-validation unsuccessfull. Please manually ensure version is correct.'
+        );
+      }
+    }
+
+    const idToUse = assetInfo?.wrappedTokenId || id; // here we are using the unwrapped id
+
+    const newAsset = {
+      ...assetInfo,
+      id,
+      address,
+      name,
+      symbol,
+      decimals,
+      version,
+
+      /* redirect the id/join if required due to using wrapped tokens */
+      joinAddress: joinMap.get(idToUse),
+      idToUse,
+
+      /* default setting of assetInfo fields if required */
+      displaySymbol: assetInfo.displaySymbol || symbol,
+      showToken: assetInfo.showToken || false,
+    };
+
+    return { ...(await assetMap), [id]: chargeAsset(newAsset) as IAssetRoot };
+  }, {});
+};
+
+/* add on extra/calculated ASSET info and contract instances  (no async) */
+const chargeAsset = (asset: any) => ({
+  ...asset,
+  digitFormat: ASSET_INFO.get(asset.id)?.digitFormat || 6,
+});
