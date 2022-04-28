50685b92196cdd081e8df53cb5f1892f59c51d3c
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-28 07:24:45 -0400

    mapify props from get static props (cant use Map in ssr)

--- src/pages/borrow/index.tsx
+++ src/pages/borrow/index.tsx
@@ -1,7 +1,29 @@
+import { ethers } from 'ethers';
+import { InferGetStaticPropsType } from 'next';
 import dynamic from 'next/dynamic';
+import { SUPPORTED_RPC_URLS } from '../../config/chainData';
+import { getAssets } from '../../lib/chain/assets';
+import { getContracts } from '../../lib/chain/contracts';
+import { getSeries } from '../../lib/chain/series';
 
 const DynamicBorrow = dynamic(() => import('../../components/views/Borrow'), { ssr: false });
 
-const Borrow = () => <DynamicBorrow />;
+const mapify = (obj: Object) =>
+  Object(obj)
+    .keys()
+    .reduce((map, curr) => map.set(curr, obj[curr]), new Map());
+
+const Borrow = ({ assetMap, seriesMap }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <DynamicBorrow assetMapProps={mapify(assetMap)} seriesMapProps={mapify(seriesMap)} />
+);
+
+export const getStaticProps = async () => {
+  const chainId = 1;
+  const provider = new ethers.providers.JsonRpcProvider(SUPPORTED_RPC_URLS[chainId], chainId);
+  const contractMap = getContracts(provider, chainId);
+  const assetMap = await getAssets(provider, contractMap);
+  const seriesMap = await getSeries(provider, chainId, contractMap);
+  return { props: { assetMap, seriesMap } };
+};
 
 export default Borrow;
