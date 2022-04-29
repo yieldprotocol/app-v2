d739a11fc70a7e206eae127096c85ea722cfbb08
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-29 13:11:00 -0400

    styling updates to include crv logo for cvx3crv

--- src/components/logos/3CRVMark.tsx
+++ src/components/logos/3CRVMark.tsx
@@ -2,31 +2,25 @@ import { Box } from 'grommet';
 import styled from 'styled-components';
 import CRVMark from './CRVMark';
 import CVXMark from './CVXMark';
+import Logo from './Logo';
 
 const Outer = styled(Box)`
   position: relative;
-`;
-
-const CVXWrap = styled(Box)`
-  position: absolute;
-  z-index: 0;
 `;
 
 const CRVWrap = styled(Box)`
   position: absolute;
   z-index: 1;
   top: -5px;
-  left: 10px;
+  left: -8px;
 `;
 
 const TriCRVCVXMark = () => (
   <Outer>
-    {/* <CVXWrap> */}
-    <CVXMark />
-    {/* </CVXWrap> */}
-    {/* <CRVWrap>
-      <CRVMark height=".75em" width=".75em" />
-    </CRVWrap> */}
+    <Logo image={<CVXMark />} />
+    <CRVWrap>
+      <Logo image={<CRVMark />} height="16px" width="16px" />
+    </CRVWrap>
   </Outer>
 );
 
