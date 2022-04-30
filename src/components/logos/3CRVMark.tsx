dc33d4a3df4fda81c8e6aa5fbcaf9da59d3f11f2
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-30 17:23:17 -0400

    add cvxcrv mark

--- src/components/logos/3CRVMark.tsx
+++ src/components/logos/3CRVMark.tsx
@@ -1,33 +1,36 @@
 import { Box } from 'grommet';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import CRVMark from './CRVMark';
 import CVXMark from './CVXMark';
+import Logo from './Logo';
 
 const Outer = styled(Box)`
   position: relative;
-`;
-
-const CVXWrap = styled(Box)`
-  position: absolute;
-  z-index: 0;
+  max-width: fit-content;
 `;
 
 const CRVWrap = styled(Box)`
   position: absolute;
-  z-index: 1;
+  z-index: 10;
   top: -5px;
-  left: 10px;
+  left: -8px;
 `;
 
-const TriCRVCVXMark = () => (
-  <Outer>
-    {/* <CVXWrap> */}
-    <CVXMark />
-    {/* </CVXWrap> */}
-    {/* <CRVWrap>
-      <CRVMark height=".75em" width=".75em" />
-    </CRVWrap> */}
-  </Outer>
-);
+const TriCRVCVXMark = () => {
+  const { pathname } = useRouter();
+  const isDash = pathname.includes('dashboard');
+  const bottomStackSize = isDash ? '16px' : '24px';
+  const topStackSize = isDash ? '12px' : '16px';
+
+  return (
+    <Outer>
+      <Logo image={<CVXMark />} height={bottomStackSize} width={bottomStackSize} />
+      <CRVWrap>
+        <Logo image={<CRVMark />} height={topStackSize} width={topStackSize} />
+      </CRVWrap>
+    </Outer>
+  );
+};
 
 export default TriCRVCVXMark;
