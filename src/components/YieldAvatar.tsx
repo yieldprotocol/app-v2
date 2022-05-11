444b66c341a9b4c2810a11a62f3ccda963ea7491
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-11 16:43:43 -0400

    feat: use ens name and avatar from wagmi and remove hook

--- src/components/YieldAvatar.tsx
+++ src/components/YieldAvatar.tsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import multiavatar from '@multiavatar/multiavatar';
 import { Avatar, Box, Image } from 'grommet';
 
+import { useEnsName } from 'wagmi';
 import { useColorScheme } from '../hooks/useColorScheme';
-import { useEns } from '../hooks/useEns';
 
 const StyledBox = styled(Box)`
   -webkit-transition: transform 0.3s ease-in-out;
@@ -16,18 +16,19 @@ const StyledBox = styled(Box)`
 
 function YieldAvatar(props: any) {
   const colorScheme = useColorScheme();
-  const { ensAvatarUrl } = useEns();
+  const { data: ensName } = useEnsName();
+  const ensAvatarImageUrl = `https://metadata.ens.domains/mainnet/avatar/${ensName}`;
   const _avatar = multiavatar(props.address.concat(21));
   const _size = props.size.toString().concat('em');
 
   return (
     <StyledBox>
       <Avatar border={{ color: colorScheme === 'dark' ? '#FFF' : '#000' }} size={_size || '2em'}>
-        <Box width="100%" height="100%" pad={ensAvatarUrl ? undefined : '2px'}>
+        <Box width="100%" height="100%" pad={ensAvatarImageUrl ? undefined : '2px'}>
           {
             // eslint-disable-next-line react/no-danger
-            ensAvatarUrl ? (
-              <Image src={ensAvatarUrl} alt="ens-avatar" />
+            ensAvatarImageUrl ? (
+              <Image src={ensAvatarImageUrl} alt="ens-avatar" />
             ) : (
               <span dangerouslySetInnerHTML={{ __html: _avatar }} />
             )
