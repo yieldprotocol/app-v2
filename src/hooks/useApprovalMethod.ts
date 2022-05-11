15e438d56051d4dad1ca2e8933dbd4372e261f12
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-11 17:17:59 -0400

    feat: use wagmi active connector name

--- src/hooks/useApprovalMethod.ts
+++ src/hooks/useApprovalMethod.ts
@@ -1,28 +1,23 @@
 import { useContext, useEffect, useState } from 'react';
-import { ChainContext } from '../contexts/ChainContext';
+import { useConnect } from 'wagmi';
 import { SettingsContext } from '../contexts/SettingsContext';
 import { ApprovalType } from '../types';
 
 export const useApprovalMethod = (): ApprovalType => {
+  const { activeConnector } = useConnect();
   const {
     settingsState: { approvalMethod },
   } = useContext(SettingsContext);
 
-  const {
-    chainState: {
-      connection: { connectionName },
-    },
-  } = useContext(ChainContext);
-
   const [approvalMethodToUse, setApprovalMethodToUse] = useState<ApprovalType>(ApprovalType.SIG);
 
   useEffect(() => {
-    if (connectionName !== 'metamask' || approvalMethod === ApprovalType.TX ) {
+    if (activeConnector.name !== 'MetaMask' || approvalMethod === ApprovalType.TX) {
       setApprovalMethodToUse(ApprovalType.TX);
     } else {
       setApprovalMethodToUse(ApprovalType.SIG);
     }
-  }, [approvalMethod, setApprovalMethodToUse, connectionName]);
+  }, [approvalMethod, setApprovalMethodToUse, activeConnector.name]);
 
   return approvalMethodToUse;
 };
