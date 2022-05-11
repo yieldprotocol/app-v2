14890411719a47fbd2fa3ee85b0770eb8d87760d
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-11 10:58:32 -0400

    add mainnet name cvx3crv mark reference

--- src/config/marks.tsx
+++ src/config/marks.tsx
@@ -29,7 +29,8 @@ const markMap = new Map([
   ['yvUSDC', <YVUSDCMark key="yvusdc" />],
   ['MKR', <MakerMark key="mkr" />],
   ['Notional', <NotionalMark key="notional" />],
-  ['Cvx3Crv Mock', <TriCRVCVXMark key="cvx3crv" />],
+  ['Cvx3Crv Mock', <TriCRVCVXMark key="cvx3crv mock" />],
+  ['cvx3Crv', <TriCRVCVXMark key="cvx3crv" />],
   ['FRAX', <FRAXMark key="frax" />],
 ]);
 
