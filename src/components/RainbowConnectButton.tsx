cb5eabc60a8bd2c956d1a43e7e05bd41960f94b1
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-16 15:40:11 -0400

    add custom rainbow connect button to be able to open settings modal on account click

---
+++ src/components/RainbowConnectButton.tsx
@@ -0,0 +1,110 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { Box, ResponsiveContext, Text } from 'grommet';
+import { useContext } from 'react';
+import { FiSettings } from 'react-icons/fi';
+import styled from 'styled-components';
+import { useAccount, useBalance } from 'wagmi';
+import { abbreviateHash } from '../utils/appUtils';
+import EthMark from './logos/EthMark';
+import Skeleton from './wraps/SkeletonWrap';
+import YieldAvatar from './YieldAvatar';
+
+const StyledText = styled(Text)`
+  svg,
+  span {
+    vertical-align: middle;
+  }
+`;
+
+const StyledBox = styled(Box)`
+  text-decoration: none;
+  padding: 8px;
+  -webkit-transition: transform 0.3s ease-in-out;
+  -moz-transition: transform 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out;
+  :hover {
+    transform: scale(1.1);
+  }
+`;
+
+interface IProps {
+  setSettingsOpen: (isOpen: boolean) => void;
+  chainOnly?: boolean;
+}
+
+const RainbowConnectButton = ({ setSettingsOpen, chainOnly }: IProps) => {
+  const mobile: boolean = useContext<any>(ResponsiveContext) === 'small';
+  const { data: _account } = useAccount();
+  const { data: ethBalance, isLoading } = useBalance({ addressOrName: _account?.address });
+
+  return (
+    <ConnectButton.Custom>
+      {({ account, chain, openAccountModal, openChainModal, openConnectModal, mounted }) => (
+        <div
+          {...(!mounted && {
+            'aria-hidden': true,
+            style: {
+              opacity: 0,
+              pointerEvents: 'none',
+              userSelect: 'none',
+            },
+          })}
+        >
+          {(() => {
+            if (!mounted || !account || !chain) {
+              return (
+                <button onClick={openConnectModal} type="button">
+                  Connect Wallet
+                </button>
+              );
+            }
+
+            if (chain.unsupported) {
+              return (
+                <button onClick={openChainModal} type="button">
+                  Wrong network
+                </button>
+              );
+            }
+
+            return (
+              <StyledBox round onClick={() => setSettingsOpen(true)} pad="xsmall" justify="center">
+                {mobile ? (
+                  <Box>
+                    <FiSettings />
+                  </Box>
+                ) : (
+                  <Box direction="row" align="center" gap="small">
+                    <Box>
+                      <Text color="text" size="small">
+                        {account.ensName || abbreviateHash(account.address, 5)}
+                      </Text>
+
+                      <Box direction="row" align="center" gap="small">
+                        <Box direction="row" gap="xsmall" align="center">
+                          <StyledText size="small" color="text">
+                            <Box height="20px" width="20px">
+                              {isLoading ? <Skeleton circle height={20} width={20} /> : <EthMark />}
+                            </Box>
+                          </StyledText>
+                          <StyledText size="small" color="text">
+                            {isLoading ? <Skeleton width={40} /> : ethBalance?.formatted}
+                          </StyledText>
+                        </Box>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <YieldAvatar address={account.address} size={2} />
+                    </Box>
+                  </Box>
+                )}
+              </StyledBox>
+            );
+          })()}
+        </div>
+      )}
+    </ConnectButton.Custom>
+  );
+};
+
+export default RainbowConnectButton;
