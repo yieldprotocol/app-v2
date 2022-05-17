2ad817271bec3bab86343dd86af91ce13637ef83
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-17 11:51:18 -0400

    fix: remove extraneous logic

--- src/components/ChangeConnectionButton.tsx
+++ src/components/ChangeConnectionButton.tsx
@@ -2,26 +2,20 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { Text } from 'grommet';
 import GeneralButton from './buttons/GeneralButton';
 
-const ChangeConnectionButton = () => (
+const ChangeConnectionButton = ({ setSettingsOpen }: { setSettingsOpen: (isOpen: boolean) => void }) => (
   <ConnectButton.Custom>
     {({ account, chain, openConnectModal, mounted, openChainModal, openAccountModal }) => (
       <>
-        {(() => {
-          if (!mounted || !account || !chain) {
-            return null;
-          }
-
-          return (
-            <GeneralButton
-              action={() => {
-                openConnectModal();
-              }}
-              background="gradient-transparent"
-            >
-              <Text size="xsmall">Change Connection</Text>
-            </GeneralButton>
-          );
-        })()}
+        {(() => (
+          <GeneralButton
+            action={() => {
+              openConnectModal();
+            }}
+            background="gradient-transparent"
+          >
+            <Text size="xsmall">Change Connection</Text>
+          </GeneralButton>
+        ))()}
       </>
     )}
   </ConnectButton.Custom>
