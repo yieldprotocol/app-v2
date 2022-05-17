f1fb93d5118a6635d33e43c924eee01de993d755
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-17 15:50:37 -0400

    fix: handle provider changes (replicate original methodology of clearing specific localStorage items and reload)

--- src/hooks/useDefaultProvider.ts
+++ src/hooks/useDefaultProvider.ts
@@ -1,16 +1,45 @@
 import { ethers } from 'ethers';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useNetwork } from 'wagmi';
 import { RPC_URLS } from '../config/chainData';
+import { clearCachedItems } from '../utils/appUtils';
 import { useCachedState } from './generalHooks';
 
 const useDefaultProvider = () => {
   const { activeChain } = useNetwork();
   const chainId = activeChain?.id;
-  const [lastChainId] = useCachedState('lastChainId', '1');
-  const chainIdToUse = chainId ?? lastChainId;
+  const [lastChainId, setLastChainId] = useCachedState('lastChainId', '1');
+  const chainIdToUse = chainId ?? +lastChainId;
 
-  return useMemo(() => new ethers.providers.StaticJsonRpcProvider(RPC_URLS[chainIdToUse]), [chainIdToUse]);
+  const _provider = useMemo(
+    () => new ethers.providers.StaticJsonRpcProvider(RPC_URLS[chainIdToUse], chainIdToUse),
+    [chainIdToUse]
+  );
+
+  /**
+   * Handle chain/provider changes
+   * Clear provider-dependent items in localStorage
+   */
+  useEffect(() => {
+    if (lastChainId && chainId !== lastChainId) {
+      clearCachedItems([
+        'lastChainId',
+        'assets',
+        'series',
+        'lastAssetUpdate',
+        'lastSeriesUpdate',
+        'strategies',
+        'lastStrategiesUpdate',
+        'connectionName',
+      ]);
+
+      setLastChainId(chainId);
+      // eslint-disable-next-line no-restricted-globals
+      location.reload();
+    }
+  }, [_provider, chainId, lastChainId, setLastChainId]);
+
+  return _provider;
 };
 
 export default useDefaultProvider;
