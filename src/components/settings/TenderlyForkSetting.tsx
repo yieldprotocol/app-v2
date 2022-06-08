77dff9d8cbe9df284595944f0e499981cc7b1ea0
marcomariscal <marco.a.mariscal@gmail.com>
2022-06-08 17:26:50 -0400

    feat: fill eth button

--- src/components/settings/TenderlyForkSetting.tsx
+++ src/components/settings/TenderlyForkSetting.tsx
@@ -1,9 +1,12 @@
 import { Box, Text } from 'grommet';
 import Switch from 'react-switch';
 import { useConnection } from '../../hooks/useConnection';
+import useTenderly from '../../hooks/useTenderly';
+import GeneralButton from '../buttons/GeneralButton';
 
 const TenderlyForkSetting = () => {
   const { connectionState, connectionActions } = useConnection();
+  const { fillEther } = useTenderly();
 
   return (
     <Box gap="small" pad={{ vertical: 'small' }}>
@@ -23,6 +26,9 @@ const TenderlyForkSetting = () => {
           borderRadius={20}
         />
       </Box>
+      <GeneralButton action={fillEther} background="background">
+        <Text size="xsmall">Fill ETH</Text>
+      </GeneralButton>
     </Box>
   );
 };
--- src/hooks/useTenderly.ts
+++ src/hooks/useTenderly.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers';
-import { useContext, useEffect } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { ChainContext } from '../contexts/ChainContext';
 import { SettingsContext } from '../contexts/SettingsContext';
 import { IChainContext, ISettingsContext } from '../types';
@@ -15,21 +15,24 @@ const useTenderly = () => {
     settingsState: { useTenderlyFork },
   } = useContext(SettingsContext) as ISettingsContext;
 
+  const fillEther = useCallback(async () => {
+    try {
+      const tenderlyProvider = new ethers.providers.JsonRpcProvider(process.env.TENDERLY_JSON_RPC_URL);
+      const transactionParameters = [[account], ethers.utils.hexValue(10000000000)];
+      console.log('🦄 ~ file: useTenderly.ts ~ line 22 ~ fillEther ~ useTenderlyFork', useTenderlyFork);
+      await tenderlyProvider?.send('tenderly_addBalance', transactionParameters);
+      const balance = await tenderlyProvider?.getBalance(account);
+      console.log('🦄 ~ file: useTenderly.ts ~ line 25 ~ fillEther ~ balance ', balance);
+    } catch (e) {
+      console.log('could not fill eth on tenderly fork');
+    }
+  }, [account, useTenderlyFork]);
+
   useEffect(() => {
-    const fillEther = async (): Promise<void> => {
-      try {
-        const transactionParameters = [[account], ethers.utils.hexValue(1000000000000000000)];
-        useTenderlyFork && (await fallbackProvider?.send('tenderly_addBalance', transactionParameters));
-
-        const balance = await fallbackProvider?.getBalance(account);
-        console.log('🦄 ~ file: useTenderly.ts ~ line 25 ~ fillEther ~ balance ', balance);
-      } catch (e) {
-        console.log('could not fill eth on tenderly fork');
-      }
-    };
-
     fillEther();
-  }, [account, fallbackProvider, useTenderlyFork]);
+  }, [fillEther]);
+
+  return { fillEther };
 };
 
 export default useTenderly;
