202f68f8bd7110c07dd28a306227115676f783b9
marcomariscal <marco.a.mariscal@gmail.com>
2022-06-09 12:06:39 -0400

    chore: clean console logs

--- src/hooks/useTenderly.ts
+++ src/hooks/useTenderly.ts
@@ -7,7 +7,7 @@ import { IChainContext, ISettingsContext } from '../types';
 const useTenderly = () => {
   const {
     chainState: {
-      connection: { fallbackProvider, account },
+      connection: { account },
     },
   } = useContext(ChainContext) as IChainContext;
 
@@ -18,15 +18,12 @@ const useTenderly = () => {
   const fillEther = useCallback(async () => {
     try {
       const tenderlyProvider = new ethers.providers.JsonRpcProvider(process.env.TENDERLY_JSON_RPC_URL);
-      const transactionParameters = [[account], ethers.utils.hexValue(10000000000)];
-      console.log('🦄 ~ file: useTenderly.ts ~ line 22 ~ fillEther ~ useTenderlyFork', useTenderlyFork);
+      const transactionParameters = [[account], ethers.utils.hexValue(1000000000)];
       await tenderlyProvider?.send('tenderly_addBalance', transactionParameters);
-      const balance = await tenderlyProvider?.getBalance(account);
-      console.log('🦄 ~ file: useTenderly.ts ~ line 25 ~ fillEther ~ balance ', balance);
     } catch (e) {
       console.log('could not fill eth on tenderly fork');
     }
-  }, [account, useTenderlyFork]);
+  }, [account]);
 
   useEffect(() => {
     fillEther();
