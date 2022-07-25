5476171866c6a8b9ca9b62984945cace3b2d713d
marcomariscal <marco.a.mariscal@gmail.com>
2022-07-25 08:28:46 -0700

    feat: add estimated fyToken out when rolling

--- src/hooks/viewHelperHooks/useLendHelpers.ts
+++ src/hooks/viewHelperHooks/useLendHelpers.ts
@@ -1,6 +1,6 @@
 import { BigNumber, ethers } from 'ethers';
 import { useContext, useEffect, useState } from 'react';
-import { maxBaseIn, maxBaseOut, maxFyTokenIn, sellBase, sellFYToken } from '@yield-protocol/ui-math';
+import { buyBase, maxBaseIn, maxBaseOut, maxFyTokenIn, sellBase, sellFYToken } from '@yield-protocol/ui-math';
 
 import { SettingsContext } from '../../contexts/SettingsContext';
 import { UserContext } from '../../contexts/UserContext';
@@ -24,23 +24,29 @@ export const useLendHelpers = (
   const { activeAccount, selectedBase } = userState;
 
   /* clean to prevent underflow */
+  const [userBaseBalance, setUserBaseBalance] = useState<BigNumber>(ethers.constants.Zero);
+
   const [maxLend, setMaxLend] = useState<BigNumber>(ethers.constants.Zero);
   const [maxLend_, setMaxLend_] = useState<string>();
 
+  /* Position state */
   const [maxClose, setMaxClose] = useState<BigNumber>(ethers.constants.Zero);
   const [maxClose_, setMaxClose_] = useState<string>();
-
-  const [maxRoll, setMaxRoll] = useState<BigNumber>(ethers.constants.Zero);
-  const [maxRoll_, setMaxRoll_] = useState<string>();
 
   const [valueAtMaturity, setValueAtMaturity] = useState<BigNumber>(ethers.constants.Zero);
   const [valueAtMaturity_, setValueAtMaturity_] = useState<string>();
 
-  const [userBaseBalance, setUserBaseBalance] = useState<BigNumber>(ethers.constants.Zero);
-
   const [protocolLimited, setProtocolLimited] = useState<boolean>(false);
 
   const [fyTokenMarketValue, setFyTokenMarketValue] = useState<string>();
+
+  /* Roll state */
+  const [maxRoll, setMaxRoll] = useState<BigNumber>(ethers.constants.Zero);
+  const [maxRoll_, setMaxRoll_] = useState<string>();
+
+  // estimated fyTokens received after rolling to a new series
+  const [rollEstimate, setRollEstimate] = useState<BigNumber>(ethers.constants.Zero);
+  const [rollEstimate_, setRollEstimate_] = useState<string>();
 
   const { apr: apy } = useApr(input, ActionType.LEND, series);
 
@@ -213,8 +219,30 @@ export const useLendHelpers = (
       diagnostics && console.log('MAXSHARES_IN', _maxSharesIn.toString());
       diagnostics && console.log('FYTOKEN_TO_BASE_VALUE', baseValue.toString());
       diagnostics && console.log('MAXSHARES_IN <= SHARES_VALUE', _maxSharesIn.lte(_sharesValue));
+
+      // estimate roll output
+      if (input) {
+        console.log('🦄 ~ file: useLendHelpers.ts ~ line 225 ~ useEffect ~ input', input);
+        const baseAmount = ethers.utils.parseUnits(input, series.decimals);
+
+        // estimate how much fyToken you can get given input (base), using the to series
+        const _fyTokenValueOfInputOut = sellBase(
+          rollToSeries.sharesReserves,
+          rollToSeries.fyTokenReserves,
+          rollToSeries.getShares(baseAmount),
+          getTimeTillMaturity(rollToSeries.maturity),
+          rollToSeries.ts,
+          rollToSeries.g1,
+          rollToSeries.decimals,
+          rollToSeries.c,
+          rollToSeries.mu
+        );
+
+        setRollEstimate(_fyTokenValueOfInputOut);
+        setRollEstimate_(ethers.utils.formatUnits(_fyTokenValueOfInputOut, rollToSeries.decimals).toString());
+      }
     }
-  }, [diagnostics, rollToSeries, series, getTimeTillMaturity]);
+  }, [diagnostics, rollToSeries, series, getTimeTillMaturity, input]);
 
   return {
     maxLend,
@@ -225,6 +253,7 @@ export const useLendHelpers = (
 
     maxRoll,
     maxRoll_,
+    rollEstimate_,
 
     apy,
     valueAtMaturity,
