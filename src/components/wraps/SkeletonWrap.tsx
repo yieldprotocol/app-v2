3588ec1a196478a3f887384b690cd0cd9666df15
marcomariscal <marco.a.mariscal@gmail.com>
2022-08-04 16:03:18 -0700

    feat: scrap the width prop

--- src/components/wraps/SkeletonWrap.tsx
+++ src/components/wraps/SkeletonWrap.tsx
@@ -6,7 +6,7 @@ const SkeletonWrap = ({ ...props }: any) => {
   const theme = useContext(ThemeContext);
   return (
     <SkeletonTheme baseColor={theme.dark ? '#202A30' : undefined} highlightColor={theme.dark ? '#313c42' : undefined}>
-      <Skeleton width={50} {...props} />
+      <Skeleton {...props} />
     </SkeletonTheme>
   );
 };
