67694ceff5460d86b7171c8a2ff87f55bc4dd443
marcomariscal <marco.a.mariscal@gmail.com>
2022-08-09 09:57:34 -0700

    feat: only tenderly in dev

--- src/components/YieldSettings.tsx
+++ src/components/YieldSettings.tsx
@@ -124,10 +124,7 @@ const YieldSettings = ({ setSettingsOpen, setConnectOpen }: any) => {
         <ApprovalSetting />
         <UnwrapSetting />
         <SlippageSetting />
-        {
-          // process.env.ENV === 'development' &&
-          <TenderlyForkSetting />
-        }
+        {process.env.ENV === 'development' && <TenderlyForkSetting />}
       </Box>
 
       <Box pad="medium" gap="small" flex={false}>
