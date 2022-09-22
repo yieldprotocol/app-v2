e0ac5d9be61b751f7e253e78ea5df233bede057d
marcomariscal <marco.a.mariscal@gmail.com>
2022-09-22 12:02:16 -0700

    chore: format

--- src/components/selectors/NetworkSelector.tsx
+++ src/components/selectors/NetworkSelector.tsx
@@ -1,24 +1,22 @@
-import { useContext, useEffect, useState } from 'react';
-import { Box, Select, Text } from 'grommet';
-import { FiChevronDown } from 'react-icons/fi';
-import { ChainContext } from '../../contexts/ChainContext';
-import { useNetworkSelect } from '../../hooks/useNetworkSelect';
-import { IChainContext } from '../../types';
-import ArbitrumLogo from '../logos/Arbitrum';
-import EthMark from '../logos/EthMark';
-import { useNetwork, useProvider, useSwitchNetwork } from 'wagmi';
+import { Text } from 'grommet';
+import { useNetwork, useProvider } from 'wagmi';
 import { useChainModal, useConnectModal } from '@rainbow-me/rainbowkit';
 
 const NetworkSelector = () => {
   const { chain } = useNetwork();
-  const  provider = useProvider();
+  const provider = useProvider();
   const { openChainModal } = useChainModal();
   const { openConnectModal } = useConnectModal();
 
-  return ( chain 
-    ? <Text size='xsmall' onClick={() => openChainModal()}> {chain?.name} </Text>
-    : <Text size='xsmall' onClick={() => openConnectModal()}> {provider.chains[0].name} </Text>
-    )
+  return chain ? (
+    <Text size="xsmall" onClick={openChainModal}>
+      {chain.name}
+    </Text>
+  ) : (
+    <Text size="xsmall" onClick={openConnectModal}>
+      {provider.chains![0].name}
+    </Text>
+  );
 };
 
 export default NetworkSelector;
