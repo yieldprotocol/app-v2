d441fcc88234a5e81efb4cb1aa0aa92c098f1f7f
marcomariscal <marco.a.mariscal@gmail.com>
2022-10-31 16:52:33 -0700

    feat: use default provider hook

---
+++ src/hooks/useDefaultProvider.ts
@@ -0,0 +1,12 @@
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { useMemo } from 'react';
+import { useNetwork, useProvider } from 'wagmi';
+
+const useDefaulProvider = () => {
+  const { chains } = useNetwork();
+  const provider = useProvider();
+
+  return useMemo(() => provider ?? new JsonRpcProvider(chains[0].rpcUrls[0]), []);
+};
+
+export default useDefaulProvider;
