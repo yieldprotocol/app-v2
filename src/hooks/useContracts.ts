6cd04a69c3d3418a7caa165af2825bac225ab515
marcomariscal <marco.a.mariscal@gmail.com>
2022-11-01 13:03:11 -0700

    feat: useContracts hook

---
+++ src/hooks/useContracts.ts
@@ -0,0 +1,22 @@
+import { Contract } from 'ethers';
+import { useMemo } from 'react';
+import yieldEnv from './../contexts/yieldEnv.json';
+import * as contractTypes from '../contracts';
+import useChainId from './useChainId';
+import useDefaultProvider from './useDefaultProvider';
+
+const useContracts = () => {
+  const { addresses } = yieldEnv;
+  const chainId = useChainId();
+  const provider = useDefaultProvider();
+  const chainAddrs = addresses[chainId];
+
+  return useMemo(() => {
+    return Object.keys(chainAddrs).reduce((contracts, name: string) => {
+      const contract = contractTypes[`${name}__factory`].connect(chainAddrs[name], provider) as Contract;
+      return contracts.set(name, contract);
+    }, new Map() as Map<string, Contract>);
+  }, [chainAddrs, provider]);
+};
+
+export default useContracts;
