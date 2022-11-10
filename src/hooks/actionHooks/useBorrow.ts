0b913bdbd309dd9470fc446294507e265b271e30
marcomariscal <marco.a.mariscal@gmail.com>
2022-11-10 11:57:42 -0800

    feat: use swr to mutate base/ilk

--- src/hooks/actionHooks/useBorrow.ts
+++ src/hooks/actionHooks/useBorrow.ts
@@ -1,3 +1,4 @@
+import { useSWRConfig } from 'swr';
 import { ethers } from 'ethers';
 import { useContext } from 'react';
 import { buyBase, calculateSlippage } from '@yield-protocol/ui-math';
@@ -18,15 +19,23 @@ import { ConvexLadleModule } from '../../contracts';
 import useTimeTillMaturity from '../useTimeTillMaturity';
 import { useAccount } from 'wagmi';
 import useContracts, { ContractNames } from '../useContracts';
+import useAsset from '../useAsset';
 
-export const useBorrow = () => {
+export const useBorrow = (vault?: IVault) => {
+  const { mutate } = useSWRConfig();
   const {
     settingsState: { slippageTolerance },
   } = useContext(SettingsContext);
 
   const { userState, userActions } = useContext(UserContext);
-  const { selectedIlk, selectedSeries, seriesMap, assetMap } = userState;
-  const { updateVaults, updateAssets, updateSeries } = userActions;
+  const { selectedIlk, selectedSeries, seriesMap } = userState;
+  const { updateVaults, updateSeries } = userActions;
+
+  /* Set the series and ilk based on the vault that has been selected or if it's a new vault, get from the globally selected SeriesId */
+  const series = vault ? seriesMap.get(vault.seriesId) : selectedSeries;
+  const { data: base, key: baseKey } = useAsset(series?.baseId!);
+  const { data: ilkToUse, key: ilkToUseKey } = useAsset(vault ? vault.ilkId : selectedIlk?.proxyId!);
+
   const { address: account } = useAccount();
   const contracts = useContracts();
 
@@ -36,19 +45,20 @@ export const useBorrow = () => {
   const { sign, transact } = useChain();
   const { getTimeTillMaturity } = useTimeTillMaturity();
 
-  const borrow = async (vault: IVault | undefined, input: string | undefined, collInput: string | undefined) => {
+  /* use the vault id provided OR 0 if new/ not provided */
+  const vaultId = vault?.id || BLANK_VAULT;
+
+  const borrow = async (input: string | undefined, collInput: string | undefined) => {
+    if (!account) throw new Error('no account detected in use borrow');
+    if (!series) throw new Error('no series detected in use borrow');
+    if (!base) throw new Error('no base detected in use borrow');
+    if (!ilkToUse) throw new Error('no ilk to use detected in use borrow');
+
     /* generate the reproducible txCode for tx tracking and tracing */
     const txCode = getTxCode(ActionCodes.BORROW, selectedSeries?.id!);
-    /* use the vault id provided OR 0 if new/ not provided */
-    const vaultId = vault?.id || BLANK_VAULT;
 
     const ladleAddress = contracts.get(ContractNames.LADLE)?.address;
-
-    /* Set the series and ilk based on the vault that has been selected or if it's a new vault, get from the globally selected SeriesId */
-    const series: ISeries = vault ? seriesMap?.get(vault.seriesId)! : selectedSeries!;
-    const base: IAsset = assetMap?.get(series.baseId)!;
-
-    const ilkToUse: IAsset = vault ? assetMap?.get(vault.ilkId)! : assetMap?.get(selectedIlk?.proxyId!)!; // note: we use the wrapped version if required
+    if (!ladleAddress) throw new Error('no ladle address detected in use borrow');
 
     /* is ETH  used as collateral */
     const isEthCollateral = ETH_BASED_ASSETS.includes(selectedIlk?.proxyId!);
@@ -79,20 +89,20 @@ export const useBorrow = () => {
     const _expectedFyTokenWithSlippage = calculateSlippage(_expectedFyToken, slippageTolerance.toString());
 
     /* if approveMAx, check if signature is required : note: getAllowance may return FALSE if ERC1155 */
-    const _allowance = await ilkToUse.getAllowance(account!, ilkToUse.joinAddress);
+    const _allowance = await ilkToUse.getAllowance(account, ilkToUse.joinAddress);
 
     const alreadyApproved = ethers.BigNumber.isBigNumber(_allowance) ? _allowance.gte(_collInput) : _allowance;
     console.log('Already approved', alreadyApproved);
 
     /* handle ETH deposit as Collateral, if required (only if collateral used is ETH-based ), else send ZERO_BN */
-    const addEthCallData: ICallData[] = addEth(isEthCollateral ? _collInput : ZERO_BN);
+    const addEthCallData = addEth(isEthCollateral ? _collInput : ZERO_BN);
     /* handle remove/unwrap WETH > if ETH is what is being borrowed */
-    const removeEthCallData: ICallData[] = removeEth(isEthBase ? ONE_BN : ZERO_BN); // (exit_ether sweeps all the eth out the ladle, so exact amount is not importnat -> just greater than zero)
+    const removeEthCallData = removeEth(isEthBase ? ONE_BN : ZERO_BN); // (exit_ether sweeps all the eth out the ladle, so exact amount is not importnat -> just greater than zero)
     /* handle wrapping of collateral if required */
-    const wrapAssetCallData: ICallData[] = await wrapAsset(_collInput, selectedIlk!, txCode); // note: selected ilk used here, not wrapped version
+    const wrapAssetCallData = await wrapAsset(_collInput, selectedIlk!, txCode); // note: selected ilk used here, not wrapped version
 
     /* Gather all the required signatures - sign() processes them and returns them as ICallData types */
-    const permitCallData: ICallData[] = await sign(
+    const permitCallData = await sign(
       [
         {
           target: ilkToUse,
@@ -161,8 +171,9 @@ export const useBorrow = () => {
       If a vault was provided, update it only,
       else update ALL vaults (by passing an empty array)
     */
+    mutate(baseKey);
+    mutate(ilkToUseKey);
     updateVaults();
-    updateAssets([base, ilkToUse, selectedIlk!]);
     updateSeries([series]);
   };
 
