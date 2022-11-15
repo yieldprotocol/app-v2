cbf2ebac5d724449f506a1ac2fd1d4f872402084
marcomariscal <marco.a.mariscal@gmail.com>
2022-11-15 10:08:08 -0800

    fix: handle useVault and mutate

--- src/hooks/actionHooks/useRollDebt.ts
+++ src/hooks/actionHooks/useRollDebt.ts
@@ -7,15 +7,17 @@ import { getTxCode } from '../../utils/appUtils';
 import { MAX_128, ZERO_BN } from '../../utils/constants';
 import useAsset from '../useAsset';
 import { useChain } from '../useChain';
+import useVault from '../useVault';
 
 /* Generic hook for chain transactions */
 export const useRollDebt = () => {
   const { mutate } = useSWRConfig();
   const {
-    userState: { selectedVault: vault, seriesMap },
+    userState: { selectedVault, seriesMap },
     userActions,
   } = useContext(UserContext);
-  const { updateVaults, updateSeries } = userActions;
+  const { updateSeries } = userActions;
+  const { data: vault, key: vaultKey } = useVault(selectedVault?.id);
   const { data: base, key: baseKey } = useAsset(vault?.baseId!);
 
   const {
@@ -29,7 +31,7 @@ export const useRollDebt = () => {
     if (!base) throw new Error('no base detected in roll debt');
 
     const txCode = getTxCode(ActionCodes.ROLL_DEBT, vault.id);
-    const hasDebt = vault.accruedArt.gt(ZERO_BN);
+    const hasDebt = vault.accruedArt.value.gt(ZERO_BN);
     const fromSeries = seriesMap.get(vault.seriesId);
 
     const calls: ICallData[] = [
@@ -49,7 +51,7 @@ export const useRollDebt = () => {
     await transact(calls, txCode);
 
     mutate(baseKey);
-    updateVaults([vault]);
+    mutate(vaultKey);
     updateSeries([fromSeries!, toSeries]);
     updateVaultHistory([vault]);
   };
