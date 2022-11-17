0f9178a74b2d2fee704cbb8466ba3a90f08bc20f
marcomariscal <marco.a.mariscal@gmail.com>
2022-11-17 11:17:37 -0800

    fix: pass in seriesId instead of all series

--- src/hooks/useApr.ts
+++ src/hooks/useApr.ts
@@ -1,34 +1,30 @@
 import { ethers } from 'ethers';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { sellBase, buyBase, calculateAPR } from '@yield-protocol/ui-math';
 
 import { ETH_BASED_ASSETS } from '../config/assets';
-import { UserContext } from '../contexts/UserContext';
-import { ActionType, ISeries } from '../types';
+import { ActionType } from '../types';
 import { cleanValue } from '../utils/appUtils';
 import useTimeTillMaturity from './useTimeTillMaturity';
 import useSeriesEntity from './useSeriesEntity';
 
 /* APR hook calculatess APR, min and max aprs for selected series and BORROW or LEND type */
-export const useApr = (input: string | undefined, actionType: ActionType, series: ISeries | null) => {
+export const useApr = (input: string | undefined, actionType: ActionType, seriesId: string) => {
   /* STATE FROM CONTEXT */
-  const { userState } = useContext(UserContext);
-  const { selectedSeries, selectedBase } = userState;
-  const { data: seriesEntity } = useSeriesEntity(series?.id);
+  const { data: seriesEntity } = useSeriesEntity(seriesId);
 
   /* HOOKS */
   const { getTimeTillMaturity } = useTimeTillMaturity();
-
-  const _selectedSeries = series || selectedSeries;
-  /* Make sure there won't be an underflow */
-  const _fallbackInput = ETH_BASED_ASSETS.includes(series?.baseId!) ? '.001' : '1';
-  const _input = Number(input) === 0 ? _fallbackInput : cleanValue(input, _selectedSeries?.decimals);
 
   /* LOCAL STATE */
   const [apr, setApr] = useState<string | undefined>();
 
   useEffect(() => {
     if (!seriesEntity) return;
+
+    /* Make sure there won't be an underflow */
+    const _fallbackInput = ETH_BASED_ASSETS.includes(seriesEntity.baseId) ? '.001' : '1';
+    const _input = Number(input) === 0 ? _fallbackInput : cleanValue(input, seriesEntity.decimals);
 
     const { sharesReserves, fyTokenReserves, ts, g1, g2, decimals, c, mu, maturity, getShares, apr } = seriesEntity;
 
@@ -65,7 +61,7 @@ export const useApr = (input: string | undefined, actionType: ActionType, series
     // figure out what to do with negative apr on borrow for tv series
     const _apr = calculateAPR(baseAmount, preview, maturity);
     _apr ? setApr(cleanValue(_apr, 2)) : setApr(apr);
-  }, [_fallbackInput, _input, actionType, getTimeTillMaturity, seriesEntity]);
+  }, [actionType, getTimeTillMaturity, input, seriesEntity]);
 
   return { apr };
 };
