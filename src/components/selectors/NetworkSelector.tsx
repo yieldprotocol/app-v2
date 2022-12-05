0c62af403fa74801a172db68ed709f276fb3c665
marcomariscal <marco.a.mariscal@gmail.com>
2022-12-05 14:06:21 -0800

    feat: update rainbow and make network selector more visible

--- src/components/selectors/NetworkSelector.tsx
+++ src/components/selectors/NetworkSelector.tsx
@@ -1,21 +1,28 @@
 import { Text } from 'grommet';
-import { useNetwork } from 'wagmi';
+import { useAccount, useNetwork } from 'wagmi';
 import { useChainModal, useConnectModal } from '@rainbow-me/rainbowkit';
+import GeneralButton from '../buttons/GeneralButton';
+import { PropsWithChildren } from 'react';
 
-const NetworkSelector = () => {
+const NetworkSelector = (props: PropsWithChildren) => {
+  const { address: account } = useAccount();
   const { chain, chains } = useNetwork();
   const { openChainModal } = useChainModal();
   const { openConnectModal } = useConnectModal();
 
-  return chain ? (
-    <Text size="xsmall" onClick={openChainModal}>
-      {chain.name}
-    </Text>
-  ) : chains.length ? (
-    <Text size="xsmall" onClick={openConnectModal}>
-      {chains[0].name}
-    </Text>
-  ) : null;
+  return account && chain ? (
+    <GeneralButton action={openChainModal} {...props}>
+      <Text size="small" onClick={openChainModal}>
+        {chain.name}
+      </Text>
+    </GeneralButton>
+  ) : (
+    <GeneralButton action={openConnectModal} {...props}>
+      <Text size="small" onClick={openConnectModal}>
+        {chains[0].name}
+      </Text>
+    </GeneralButton>
+  );
 };
 
 export default NetworkSelector;
--- src/components/settings/NetworkSetting.tsx
+++ src/components/settings/NetworkSetting.tsx
@@ -1,23 +1,13 @@
 import { Box, Text } from 'grommet';
 import NetworkSelector from '../selectors/NetworkSelector';
-import { useAccount } from 'wagmi';
-import BoxWrap from '../wraps/BoxWrap';
 
 const AdvancedSetting = () => {
-  const { connector } = useAccount();
-
   return (
     <Box direction="row" justify="between">
       <Box alignSelf="center">
-        <Text size="small" >
-          Connected Network 
-        </Text>
+        <Text size="small">Connected Network</Text>
       </Box>
-      <BoxWrap>
-      <Box pad={{ vertical: "xsmall", horizontal: "small" }} round background="#BFDBFE" fill="vertical">
-        <NetworkSelector />
-      </Box>
-      </BoxWrap>
+      <NetworkSelector background="#BFDBFE" />
     </Box>
   );
 };
