5f801f47467deffb0b7b76b708a62a8e8354fa40
brucedonovan <bruce.donovan@gmail.com>
2023-01-11 18:58:29 +0000

    allowunassociated v1 strategies

--- src/components/views/Pool.tsx
+++ src/components/views/Pool.tsx
@@ -78,13 +78,13 @@ function Pool() {
 
     setPoolDisabled(true);
     /* this is simply a check that we don't add to a v1 strategy that has a v2 verison) */
-    console.log('Strategy: ', selectedStrategy )
-    if (selectedStrategy.type === StrategyType.V1 && selectedStrategy.associatedStrategy ) {
-      console.log( 'config error: Trying to add to an old strategy')
-      toast.warning('Trying to add Liquidity to an old Strategy version. Please refresh your browser and try again.')
+    console.log('Strategy: ', selectedStrategy);
+    if (selectedStrategy.type === StrategyType.V1 && selectedStrategy.associatedStrategy) {
+      console.log('config error: Trying to add to an old strategy');
+      toast.warning('Trying to add Liquidity to an old Strategy version. Please refresh your browser and try again.');
       return;
-    };
- 
+    }
+
     addLiquidity(
       poolInput,
       selectedStrategy,
@@ -317,7 +317,11 @@ function Pool() {
               secondary
               label={<Text size={mobile ? 'small' : undefined}>Next Step</Text>}
               onClick={() => handleNavAction(stepPosition + 1)}
-              disabled={stepDisabled || !selectedStrategy || selectedStrategy.type === StrategyType.V1}
+              disabled={
+                stepDisabled ||
+                !selectedStrategy ||
+                (selectedStrategy.type === StrategyType.V1 && !selectedStrategy.associatedStrategy) 
+              }
               errorLabel={poolError}
             />
           )}
