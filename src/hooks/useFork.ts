dffdbe495bfd630d587f94cc1957ba923b194e2d
marcomariscal <marco.a.mariscal@gmail.com>
2023-02-17 11:27:39 -0800

    chore: remove unused and only get fork data if provider

--- src/hooks/useFork.ts
+++ src/hooks/useFork.ts
@@ -10,31 +10,27 @@ const useFork = () => {
   } = useContext(SettingsContext);
 
   const { address: account } = useAccount();
-  const provider = useMemo(() => new ethers.providers.JsonRpcProvider(forkUrl), [forkUrl]);
-
-  const getForkStartBlock = useCallback(async () => {
-    try {
-      const num = await provider.send('tenderly_getForkBlockNumber', []);
-      const sBlock = +num.toString();
-      console.log('Fork start block: ', sBlock);
-      return sBlock;
-    } catch (e) {
-      console.log('Could not get tenderly start block: ', e);
-      return 0;
-    }
-  }, [provider]);
+  const provider = useMemo(
+    () => (useForkedEnv ? new ethers.providers.JsonRpcProvider(forkUrl) : undefined),
+    [forkUrl, useForkedEnv]
+  );
 
   const getForkTimestamp = useCallback(async () => {
+    if (!provider) return undefined;
+
     try {
       const { timestamp } = await provider.getBlock('latest');
       useForkedEnv && console.log('Updated Forked Blockchain time: ', new Date(timestamp * 1000));
       return timestamp;
     } catch (e) {
       console.log('Error getting latest timestamp', e);
+      return undefined;
     }
   }, [provider, useForkedEnv]);
 
   const fillEther = useCallback(async () => {
+    if (!provider) return;
+
     if (useForkedEnv) {
       try {
         const transactionParameters = [[account], ethers.utils.hexValue(BigInt('100000000000000000000'))];
@@ -46,16 +42,13 @@ const useFork = () => {
   }, [account, provider, useForkedEnv]);
 
   const { data: forkTimestamp } = useSWRImmutable(useForkedEnv ? 'forkTimestamp' : null, getForkTimestamp);
-  const { data: forkStartBlock } = useSWRImmutable(useForkedEnv ? 'forkStartBlock' : null, getForkStartBlock);
 
   return {
     isFork: useForkedEnv,
-    getForkStartBlock,
     fillEther,
     forkUrl,
     getForkTimestamp,
     forkTimestamp,
-    forkStartBlock,
   };
 };
 
