62063fe3fda4f778f1e2f8a4cb271beeb84c10f5
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-08 11:03:11 -0800

    fix: handle series map with hook

--- src/components/DashboardPositionListItem.tsx
+++ src/components/DashboardPositionListItem.tsx
@@ -5,16 +5,19 @@ import { UserContext } from '../contexts/UserContext';
 import VaultItem from './positionItems/VaultItem';
 import LendItem from './positionItems/LendItem';
 import StrategyItem from './positionItems/StrategyItem';
+import useSeriesEntities from '../hooks/useSeriesEntities';
 
 function DashboardPositionListItem({ item, index, actionType }: { item: any; index: number; actionType: ActionType }) {
   const {
-    userState: { vaultMap, seriesMap, strategyMap },
+    userState: { vaultMap, strategyMap },
   } = useContext(UserContext);
-
+  const {
+    seriesEntities: { data: seriesMap },
+  } = useSeriesEntities();
   return (
     <Box>
       {vaultMap?.has(item.id!) && <VaultItem vault={item!} index={index} condensed />}
-      {seriesMap?.has(item.id) && <LendItem series={item!} index={index} actionType={actionType} condensed />}
+      {seriesMap?.has(item.id) && <LendItem seriesId={item.id} index={index} actionType={actionType} condensed />}
       {strategyMap?.has(item.id) && <StrategyItem strategy={item!} index={index} condensed />}
     </Box>
   );
