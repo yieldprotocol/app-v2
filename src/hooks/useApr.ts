1dca532e7153329430a6b1d70b2419b65d481103
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-08 10:52:11 -0800

    fix: use previous version, but using new series hook

--- src/hooks/useApr.ts
+++ src/hooks/useApr.ts
@@ -2,68 +2,69 @@ import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import { sellBase, buyBase, calculateAPR } from '@yield-protocol/ui-math';
 
+import { ETH_BASED_ASSETS } from '../config/assets';
 import { ActionType } from '../types';
 import { cleanValue } from '../utils/appUtils';
 import useTimeTillMaturity from './useTimeTillMaturity';
 import useSeriesEntities from './useSeriesEntities';
 
-/* APR hook estimates borrow and lend APR's for a specific series entity */
-export const useApr = (input: string | undefined, actionType: ActionType, seriesId: string | null | undefined) => {
+/* APR hook calculatess APR, min and max aprs for selected series and BORROW or LEND type */
+export const useApr = (input: string | undefined, actionType: ActionType, seriesId: string | null) => {
   const {
     seriesEntity: { data: seriesEntity },
   } = useSeriesEntities(seriesId);
+
+  /* HOOKS */
   const { getTimeTillMaturity } = useTimeTillMaturity();
 
+  /* Make sure there won't be an underflow */
+  const _fallbackInput = ETH_BASED_ASSETS.includes(seriesEntity?.baseId!) ? '0.01' : '1';
+  const _input = Number(input) === 0 ? _fallbackInput : cleanValue(input, seriesEntity?.decimals);
+
   /* LOCAL STATE */
-  const [apr, setApr] = useState<string | undefined>(() =>
-    cleanValue(actionType === ActionType.LEND ? seriesEntity?.lendAPR : seriesEntity?.borrowAPR, 2)
-  );
+  const [apr, setApr] = useState<string | undefined>();
 
   useEffect(() => {
-    if (!input || input === '') return;
-    if (!seriesEntity) return;
-
-    /* Make sure there won't be an underflow */
-    const _input = cleanValue(input, seriesEntity.decimals);
-
     let preview: ethers.BigNumber | Error = ethers.constants.Zero;
 
-    const { sharesReserves, fyTokenReserves, decimals, getShares, maturity, ts, g1, g2, c, mu } = seriesEntity;
-    const baseAmount = ethers.utils.parseUnits(_input, decimals);
+    if (seriesEntity) {
+      const { poolSymbol, sharesReserves, fyTokenReserves, ts, g1, g2, decimals, c, mu, maturity, getShares } =
+        seriesEntity;
 
-    if (actionType === ActionType.LEND) {
-      preview = sellBase(
-        sharesReserves,
-        fyTokenReserves,
-        getShares(baseAmount), // convert input from base to shares
-        getTimeTillMaturity(maturity),
-        ts,
-        g1,
-        decimals,
-        c,
-        mu
-      );
+      const baseAmount = ethers.utils.parseUnits(_input || _fallbackInput, decimals);
+
+      if (actionType === ActionType.LEND) {
+        preview = sellBase(
+          sharesReserves,
+          fyTokenReserves,
+          getShares(baseAmount), // convert input from base to shares
+          getTimeTillMaturity(maturity),
+          ts,
+          g1,
+          decimals,
+          c,
+          mu
+        );
+      }
+
+      if (actionType === ActionType.BORROW) {
+        preview = buyBase(
+          sharesReserves,
+          fyTokenReserves,
+          getShares(baseAmount), // convert input from base to shares
+          getTimeTillMaturity(maturity),
+          ts,
+          g2,
+          decimals,
+          c,
+          mu
+        );
+      }
+      // figure out what to do with negative apr on borrow for tv series
+      const _apr = calculateAPR(baseAmount, preview, maturity);
+      _apr ? setApr(cleanValue(_apr, 2)) : setApr(apr);
     }
-
-    if (actionType === ActionType.BORROW) {
-      preview = buyBase(
-        sharesReserves,
-        fyTokenReserves,
-        getShares(baseAmount), // convert input from base to shares
-        getTimeTillMaturity(maturity),
-        ts,
-        g2,
-        decimals,
-        c,
-        mu
-      );
-    }
-
-    // figure out what to do with negative apr on borrow for tv series
-    const _apr = calculateAPR(baseAmount, preview, maturity);
-
-    _apr && setApr(cleanValue(_apr, 2));
-  }, [actionType, getTimeTillMaturity, input, seriesEntity]);
+  }, [_fallbackInput, _input, actionType, apr, getTimeTillMaturity, seriesEntity]);
 
   return { apr };
 };
