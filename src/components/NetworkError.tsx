b9af145d48ed30a4962a942c8f40d67d93745935
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-09 13:05:22 -0800

    fix: handle unsupported chain

--- src/components/NetworkError.tsx
+++ src/components/NetworkError.tsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from 'react';
 import { Box, Button, Layer, Text } from 'grommet';
 import { FiAlertCircle } from 'react-icons/fi';
-import { useConnect, useDisconnect } from 'wagmi';
+import { useConnect, useDisconnect, useNetwork } from 'wagmi';
 
 const NetworkError = () => {
   const { error } = useConnect();
+  const { chain, chains } = useNetwork();
   const { disconnect } = useDisconnect();
 
   const [showError, setShowError] = useState<boolean>(false);
 
   useEffect(() => {
-    error?.message ? setShowError(true) : setShowError(false);
-  }, [error]);
+    error?.message || chain?.unsupported ? setShowError(true) : setShowError(false);
+  }, [chain?.unsupported, error]);
 
   return (
     <>
       {showError && (
         <Layer>
           <Box pad="medium" round="small" gap="small" align="center" width="600px">
-            <FiAlertCircle size="2em" /> <Text size="large">Oops. There was a connection error.</Text>
+            {chain?.unsupported && (
+              <>
+                <FiAlertCircle size="2em" />{' '}
+                <Text size="large">
+                  Chain with id {chain.id} is unsupported. Please make sure you are on a supported network.
+                </Text>
+              </>
+            )}
+            {error && (
+              <>
+                <FiAlertCircle size="2em" /> <Text size="large">Oops. There was a connection error.</Text>
+              </>
+            )}
             <Text size="small">{error?.message}</Text>
             <Button
               label="Continue without connecting a wallet"
