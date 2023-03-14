a3fb4f2015338b7ad0647390b7bad2e8e0cd9946
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-14 12:20:10 -0700

    feat: use callback and use default provider

--- src/hooks/useFork.ts
+++ src/hooks/useFork.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { ethers } from 'ethers';
 import { useCallback, useContext, useMemo } from 'react';
 import { SettingsContext } from '../contexts/SettingsContext';
@@ -5,30 +6,28 @@ import useAccountPlus from './useAccountPlus';
 import useSWRImmutable from 'swr/immutable';
 import { useBalance } from 'wagmi';
 import { toast } from 'react-toastify';
-
-import axios from 'axios';
 import useChainId from './useChainId';
-import { useNetwork, useProvider } from 'wagmi';
+import useDefaultProvider from './useDefaultProvider';
 
 const useFork = () => {
   const {
-
-    settingsState: { useForkedEnv, forkEnvUrl: forkUrl,diagnostics },
+    settingsState: { useForkedEnv, forkEnvUrl: forkUrl, diagnostics },
   } = useContext(SettingsContext);
 
   const { address: account } = useAccountPlus();
   const { refetch } = useBalance({ address: account });
-    
+
   const chainId = useChainId();
-  
+  const defaultProvider = useDefaultProvider();
+
   const provider = useMemo(
     () => (useForkedEnv ? new ethers.providers.JsonRpcProvider(forkUrl) : undefined),
     [forkUrl, useForkedEnv]
   );
-  
-    const createNewFork = async (): Promise<string> => {
+
+  const createNewFork = useCallback(async (): Promise<string> => {
     const TENDERLY_FORK_API = `http://api.tenderly.co/api/v1/account/${process.env.TENDERLY_USER}/project/${process.env.TENDERLY_PROJECT}/fork`;
-    const currentBlockNumber = await provider?.getBlockNumber();
+    const currentBlockNumber = await defaultProvider.getBlockNumber();
     const resp = await axios.post(
       TENDERLY_FORK_API,
       { network_id: chainId.toString(), block_number: currentBlockNumber },
@@ -39,12 +38,11 @@ const useFork = () => {
       }
     );
     return `https://rpc.tenderly.co/fork/${resp.data.simulation_fork.id}`;
-  };
-  
+  }, [chainId, defaultProvider]);
 
   const getForkTimestamp = useCallback(async () => {
     if (!useForkedEnv || !provider) return;
-    
+
     try {
       const { timestamp } = await provider.getBlock('latest');
       console.log('Updated Forked Blockchain time: ', new Date(timestamp * 1000));
@@ -59,20 +57,17 @@ const useFork = () => {
     if (!useForkedEnv || !provider) return 'earliest';
 
     try {
-
       const num = await provider.send('tenderly_getForkBlockNumber', []);
       const sBlock = +num.toString();
       console.log('Fork start block: ', sBlock);
       return sBlock;
 
-/** from createFork
+      /** from createFork
       //const { timestamp } = await forkProvider.getBlock('latest');
       //diagnostics && useForkedEnv && console.log('Updated Forked Blockchain time: ', new Date(timestamp * 1000));
       //setForkTimestamp(timestamp);
       //return timestamp;
       */
-
-
     } catch (e) {
       console.log('Could not get tenderly start block: ', e);
       return 'earliest';
@@ -107,9 +102,8 @@ const useFork = () => {
     forkTimestamp,
     forkStartBlock,
     createNewFork,
-    provider
+    provider,
   };
 };
 
 export default useFork;
-
