2e4c1ab36368b03926128e32151a1bac64b41ea9
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-12 11:38:44 -0700

    chore: clean

--- src/components/selectors/VariableRate.tsx
+++ src/components/selectors/VariableRate.tsx
@@ -1,16 +1,8 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Avatar, Box, Text } from 'grommet';
-import { toast } from 'react-toastify';
-import { FiAlertTriangle, FiSlash } from 'react-icons/fi';
-
 import styled from 'styled-components';
-import { ActionCodes, IStrategy } from '../../types';
 import { UserContext } from '../../contexts/UserContext';
-import { formatStrategyName } from '../../utils/appUtils';
-import Skeleton from '../wraps/SkeletonWrap';
 import { SettingsContext } from '../../contexts/SettingsContext';
-import { ZERO_BN } from '../../utils/constants';
-import useStrategyReturns, { IReturns } from '../../hooks/useStrategyReturns';
 import YieldMark from '../logos/YieldMark';
 
 const StyledBox = styled(Box)`
@@ -76,29 +68,15 @@ const ShineyBox = styled(Box)`
   }
 `;
 
-const VariableRate = (
-  //   {
-  //   strategy,
-  //   selected,
-  //   displayName,
-  //   handleClick,
-  //   returns,
-  // }: {
-  //   strategy: IStrategy;
-  //   selected: boolean;
-  //   displayName: string;
-  //   handleClick: (strategy: IStrategy) => void;
-  //   returns?: IReturns;
-  //   }
-  props: any
-) => {
+const VariableRate = () => {
   const {
     settingsState: { darkMode },
   } = useContext(SettingsContext);
 
-  const { userState, userActions } = useContext(UserContext);
-
-  const { selectedVR } = userState;
+  const {
+    userState: { selectedVR },
+    userActions: { setSelectedVR, setSelectedSeries },
+  } = useContext(UserContext);
 
   return (
     <StyledBox
@@ -106,11 +84,7 @@ const VariableRate = (
       round="large"
       background={selectedVR ? 'purple' : '#00000007'}
       elevation="xsmall"
-      onClick={() => {
-        userActions.setSelectedSeries(null);
-        userActions.setSelectedVR(true);
-        console.log('vr click', selectedVR);
-      }}
+      onClick={() => setSelectedVR(true)}
       className="VR-container"
     >
       <Box pad="medium" width="small" direction="row" gap="small" fill>
@@ -128,19 +102,16 @@ const VariableRate = (
           <Box align="center" fill="vertical" justify="center">
             <Box direction="row">
               <Text size="small" color={'text-weak'}>
-                {/* {formatStrategyName(strategy.name!)} */}
                 Variable Rate
               </Text>
             </Box>
 
             <Text size="xsmall" color={'text-weak'}>
-              {/* Rolling {displayName} */}
               Indefinite
             </Text>
           </Box>
         </Box>
 
-        {/* {strategy.rewardsRate!.gt(ZERO_BN) && ( */}
         <ShineyBox
           round
           background="-webkit-linear-gradient(95deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82)"
@@ -152,26 +123,14 @@ const VariableRate = (
             NEW
           </Text>
         </ShineyBox>
-        {/* )} */}
 
-        {/* {returns?.blendedAPY && ( */}
         <Box fill align="end" width="small" className="thisHereBox">
-          {/* <Avatar background={'linear-gradient(90deg, rgba(30,42,217,1) 15%, rgba(121,0,255,1) 78%)'} style={{}}> */}
-          {/* {strategy.currentSeries?.allowActions.includes('allow_all') ||
-              strategy.currentSeries?.allowActions.includes(ActionCodes.ADD_LIQUIDITY) ? ( */}
           <Box style={{ marginTop: 'auto', marginBottom: 'auto' }}>
             <Text size="1.5em" color={darkMode ? 'white' : 'black'}>
               {2.7} <Text size="small">% {'APR'}</Text>
             </Text>
           </Box>
-          {/* // ) : ( */}
-          {/* <Text color="red">
-              <FiAlertTriangle />
-            </Text> */}
-          {/* )} */}
-          {/* </Avatar> */}
         </Box>
-        {/* )} */}
       </Box>
     </StyledBox>
   );
