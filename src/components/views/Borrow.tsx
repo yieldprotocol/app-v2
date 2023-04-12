19d242cbc3e12a1a822bab6d0ba2fb384afbdea8
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-12 12:36:45 -0700

    fix: checking vr bases

--- src/components/views/Borrow.tsx
+++ src/components/views/Borrow.tsx
@@ -373,7 +373,7 @@ const Borrow = () => {
                       >
                         <SeriesSelector inputValue={borrowInput} actionType={ActionType.BORROW} />
                       </SectionWrap>
-                      {basesVR?.length && basesVR.includes(selectedBase.id)&& (
+                      {basesVR?.length && basesVR.includes(selectedBase?.id!) && (
                         <SectionWrap title="OR choose a variable rate">
                           <VariableRate />
                         </SectionWrap>
