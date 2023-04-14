3192137f15b4c7d72f1cc5a708dc7f783748c66f
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-14 10:09:14 -0700

    feat: use fr or vr lend based on selectedVR

--- src/hooks/actionHooks/useLend/index.ts
+++ src/hooks/actionHooks/useLend/index.ts
@@ -7,9 +7,12 @@ import { useContext } from 'react';
 import { UserContext } from '../../../contexts/UserContext';
 
 export const useLend = () => {
-  console.log('useLend firing index.ts');
+  const lendFR = useLendFR();
+  const lendVR = useLendVR();
   const { userState } = useContext(UserContext);
   const { selectedVR } = userState;
 
-  return selectedVR ? useLendVR() : useLendFR();
+  return selectedVR ? lendVR : lendFR;
 };
+
+export default useLend;
