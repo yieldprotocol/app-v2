2d85bc2bd03888681131e5a8e74a9b91409f9f2d
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-19 11:28:32 -0700

    fix: handle init types

--- src/contexts/MutlicallContext.tsx
+++ src/contexts/MutlicallContext.tsx
@@ -3,14 +3,20 @@ import { MulticallService } from '@yield-protocol/ui-multicall/src';
 import useDefaultProvider from '../hooks/useDefaultProvider';
 import useFork from '../hooks/useFork';
 
-const MulticallContext = createContext({});
+const MulticallContext = createContext<{
+  multicall: MulticallService | undefined;
+  forkMulticall: MulticallService | undefined;
+}>({ multicall: undefined, forkMulticall: undefined });
 
 const MulticallProvider = ({ children }: { children: ReactNode }) => {
   const provider = useDefaultProvider();
-  const { provider: forkProvider } = useFork();
+  const { provider: forkProvider, useForkedEnv } = useFork();
 
   const multicall = new MulticallService(provider);
   const forkMulticall = new MulticallService(forkProvider!);
+
+  if (!multicall) throw new Error('Multicall not initialized');
+  if (useForkedEnv && !forkMulticall) throw new Error('Forked Multicall not initialized');
 
   return <MulticallContext.Provider value={{ multicall, forkMulticall }}>{children}</MulticallContext.Provider>;
 };
