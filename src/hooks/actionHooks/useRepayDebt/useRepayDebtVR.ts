d606280824f09e9af5a1e2486b71eba36296d06a
marcomariscal <marco.a.mariscal@gmail.com>
2023-05-10 07:20:48 -0700

    fix: handle repaying vr

--- src/hooks/actionHooks/useRepayDebt/useRepayDebtVR.ts
+++ src/hooks/actionHooks/useRepayDebt/useRepayDebtVR.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers';
 import { useContext } from 'react';
 import { calculateSlippage, MAX_256 } from '@yield-protocol/ui-math';
-
 import { UserContext } from '../../../contexts/UserContext';
 import { ICallData, IVault, ActionCodes, LadleActions, IAsset, RoutedActions } from '../../../types';
 import { cleanValue, getTxCode } from '../../../utils/appUtils';
@@ -17,7 +16,8 @@ import useContracts from '../../useContracts';
 import useChainId from '../../useChainId';
 import useAccountPlus from '../../useAccountPlus';
 import { ContractNames } from '../../../config/contracts';
-import useAllowAction from '../../useAllowAction';
+import { mutate } from 'swr';
+import useVaultsVR from '../../entities/useVaultsVR';
 
 export const useRepayDebtVR = () => {
   const {
@@ -40,8 +40,8 @@ export const useRepayDebtVR = () => {
     token: selectedBase?.id === WETH ? undefined : (selectedBase?.address as Address),
   });
 
+  const { key: vaultsKey } = useVaultsVR();
   const { addEth, removeEth } = useAddRemoveEth();
-  const { unwrapAsset } = useWrapUnwrapAsset();
   const { sign, transact } = useChain();
   const chainId = useChainId();
 
@@ -51,80 +51,44 @@ export const useRepayDebtVR = () => {
    * @param input
    * @param reclaimCollateral
    */
-  const repayVariableRate = async (vault: IVault, input: string | undefined, reclaimCollateral: boolean) => {
-    if (!contracts) return;
+  const repay = async (vault: IVault, input: string | undefined, reclaimCollateral: boolean) => {
+    if (!contracts || !input || !assetMap || !account) return;
 
     const txCode = getTxCode(ActionCodes.REPAY, vault.id);
 
     const ladleAddress = contracts.get(ContractNames.VR_LADLE)?.address;
+    if (!ladleAddress) return console.error('Ladle address not found');
 
-    const vrCauldronAddress = contracts.get(ContractNames.VR_CAULDRON)?.address;
-    // const series: ISeries = seriesMap?.get(vault.seriesId)!;
-    const base: IAsset = assetMap?.get(vault.baseId)!;
-    const ilk: IAsset = assetMap?.get(vault.ilkId)!;
+    const base = assetMap.get(vault.baseId);
+    const ilk = assetMap.get(vault.ilkId);
 
-    // if (!isActionAllowed(ActionCodes.REPAY)) return; // return if action is not allowed
+    if (!base || !ilk) return console.error('Base or ilk not found');
 
     const isEthCollateral = ETH_BASED_ASSETS.includes(vault.ilkId);
-    const isEthBase = ETH_BASED_ASSETS.includes(base.id);
-
-    /* is convex-type collateral */
-    const isConvexCollateral = CONVEX_BASED_ASSETS.includes(ilk.proxyId);
-    const convexJoinContract = ConvexJoin__factory.connect(ilk.joinAddress, provider);
+    const isEthBase = ETH_BASED_ASSETS.includes(vault.baseId);
 
     /* Parse inputs */
     const cleanInput = cleanValue(input, base.decimals);
-    const _input = input ? ethers.utils.parseUnits(cleanInput, base.decimals) : ethers.constants.Zero;
-
-    /*
-        we won't be able to do the below maxSharesIn calculation for VR because
-        theres no series. Will need to follow up on what to do here - jacob b
-
-        can call Cauldron.balances here to get the debt
-        call when selects user vault - also worth it to try 
-        and calcultate in the UI
-    */
-
-    /* Check if the trade of that size is possible */
-    // assuming all trades are possible for now, but will need to revisit - jacob b
-    const tradeIsNotPossible = false;
-
-    diagnostics && tradeIsNotPossible ? console.log('Trade is not possible:') : console.log('Trade is possible:');
-    diagnostics && tradeIsNotPossible && console.log('Trade input', _input.toString());
-
-    const _inputAsFyTokenWithSlippage = calculateSlippage(
-      // remove fyToken references - jacob b
-      _input,
-      slippageTolerance.toString(),
-      true // minimize
-    );
+    const _input = ethers.utils.parseUnits(cleanInput, base.decimals);
 
     /* Check if input is more than the debt */
-    const inputGreaterThanEqualDebt: boolean = ethers.BigNumber.from(_input).gte(vault.accruedArt);
+    const inputGreaterThanEqualDebt = _input.gte(vault.accruedArt);
 
     /* If requested, and all debt will be repaid, automatically remove collateral */
     const _collateralToRemove =
       reclaimCollateral && inputGreaterThanEqualDebt ? vault.ink.mul(-1) : ethers.constants.Zero;
 
-    /* Cap the amount to transfer: check that if input is greater than debt, used after maturity only repay the max debt (or accrued debt) */
+    /* Cap the amount of debt to repay at total debt */
     const _inputCappedAtArt = vault.art.gt(ZERO_BN) && vault.art.lte(_input) ? vault.art : _input;
 
-    /* Set the amount to transfer ( + 0.1% after maturity ) */
-    /* assuming mature here as well - is this right? - jacob b */
-    const amountToTransfer = _input.mul(10001).div(10000);
-
-    /* In low liq situations/or mature, send repay funds to join not pool */
-    /* I believe we transfer our repayment directly to the ladle, could be wrong here - jacob b */
-    // maybe the cauldron directly? - jacob b
-    const transferToAddress = vrCauldronAddress;
+    /* Set the amount of base to transfer for repay with slight buffer */
+    const amountToTransfer = _inputCappedAtArt.mul(10001).div(10000);
 
     /* Check if already approved */
-    const alreadyApproved = (await base.getAllowance(account!, ladleAddress!)).gte(amountToTransfer);
-
-    // const wrapAssetCallData : ICallData[] = await wrapAsset(ilk, account!);
-    const unwrapAssetCallData: ICallData[] = reclaimCollateral ? await unwrapAsset(ilk, account!) : [];
+    const alreadyApproved = (await base.getAllowance(account, ladleAddress)).gte(amountToTransfer);
 
     const approveAmount = base.id === USDT && chainId !== 42161 ? MAX_256 : amountToTransfer.mul(110).div(100);
+
     const permitCallData: ICallData[] = await sign(
       [
         {
@@ -137,64 +101,45 @@ export const useRepayDebtVR = () => {
       txCode
     );
 
-    /* Remove ETH collateral. (exit_ether sweeps all the eth out of the ladle, so exact amount is not importnat -> just greater than zero) */
-    const removeEthCallData = isEthCollateral ? removeEth(ONE_BN) : [];
+    const removeEthCallData = isEthCollateral ? removeEth(ONE_BN, ladleAddress) : [];
 
-    /* Address to send the funds to either ladle (if eth is used as collateral) or account */
-    // TODO - Need to test with this after we get a VR fork with WETH module - jacob b
-    const reclaimToAddress = () => {
-      if (isEthCollateral) return ladleAddress;
-      if (unwrapAssetCallData.length && ilk.unwrapHandlerAddresses?.has(chain?.id!))
-        return ilk.unwrapHandlerAddresses?.get(chain?.id!); // if there is somethign to unwrap
-      return account;
-    };
+    /* Address to send the collateral to either ladle (if eth is used as collateral) or account */
+    const reclaimCollatToAddress = isEthCollateral ? ladleAddress : account;
 
     const calls: ICallData[] = [
       ...permitCallData,
-
-      /* Reqd. when we have a wrappedBase */
-      // ...wrapAssetCallData
-
-      /* If ethBase, Send ETH to either base join or pool  */
-      ...addEth(isEthBase ? amountToTransfer : ZERO_BN, transferToAddress), // destination = either join or series depending if tradeable
-      ...addEth(isEthBase ? amountToTransfer : ZERO_BN), // no destination defined after maturity , input +1% will will go to weth join
-
-      /* Else, Send Token to either join or pool via a ladle.transfer() */
+      ...(isEthBase ? addEth(amountToTransfer, ladleAddress) : []),
 
       {
         operation: LadleActions.Fn.TRANSFER,
-        args: [base.address, transferToAddress, amountToTransfer] as LadleActions.Args.TRANSFER,
+        args: [base.address, ladleAddress, amountToTransfer] as LadleActions.Args.TRANSFER,
         ignoreIf: isEthBase,
       },
 
-      /* convex-type collateral; ensure checkpoint before giving collateral back to account */
-      {
-        operation: LadleActions.Fn.ROUTE,
-        args: [vault.owner] as RoutedActions.Args.CHECKPOINT,
-        fnName: RoutedActions.Fn.CHECKPOINT,
-        targetContract: convexJoinContract, // use the convex join contract to checkpoint
-        ignoreIf: !isConvexCollateral || _collateralToRemove.eq(ethers.constants.Zero),
-      },
-
-      /* BEFORE MATURITY - !series.seriesIsMature */
       {
         operation: LadleActions.Fn.REPAY,
-        args: [vault.id, account, ladleAddress, _inputAsFyTokenWithSlippage] as LadleActions.Args.REPAY,
-        ignoreIf: inputGreaterThanEqualDebt || tradeIsNotPossible,
+        args: [
+          vault.id,
+          reclaimCollatToAddress,
+          reclaimCollatToAddress,
+          _collateralToRemove,
+        ] as LadleActions.Args.REPAY_VR,
+        ignoreIf: false,
       },
 
       ...removeEthCallData,
-      ...unwrapAssetCallData,
     ];
+
     await transact(calls, txCode);
 
     if (selectedBase?.proxyId !== WETH) refetchBaseBal();
     if (selectedIlk?.proxyId !== WETH) refetchIlkBal();
 
-    updateVaults([vault]);
     updateAssets([base, ilk, userState.selectedIlk!]);
-    // updateSeries([series]);
+    mutate(vaultsKey);
+
+    // TODO update vault history
   };
 
-  return repayVariableRate;
+  return repay;
 };
