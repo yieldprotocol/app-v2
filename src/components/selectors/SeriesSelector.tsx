5cd4aabe7c029a6d9a8e3354a629f3c6a4d522da
marcomariscal <marco.a.mariscal@gmail.com>
2023-05-10 05:57:14 -0700

    fix: deps

--- src/components/selectors/SeriesSelector.tsx
+++ src/components/selectors/SeriesSelector.tsx
@@ -184,15 +184,12 @@ function SeriesSelector({ selectSeriesLocally, inputValue, actionType, cardLayou
 
     setOptions(filteredOpts.sort((a, b) => a.maturity - b.maturity));
   }, [
-    seriesMap,
-    selectedBase,
     selectSeriesLocally,
-    _selectedSeries,
-    userActions,
-    selectedSeries,
-    actionType,
-    selectedVault,
-    options,
+    selectedBase?.proxyId,
+    selectedSeries?.baseId,
+    selectedSeries?.id,
+    selectedSeries?.maturity,
+    seriesMap,
   ]);
 
   const handleSelect = (_series: ISeries) => {
