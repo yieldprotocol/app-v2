fac94d2b3c307a6aaf77364bc60249bdb3bbfbad
marcomariscal <marco.a.mariscal@gmail.com>
2023-05-12 02:28:34 -0700

    feat: vyToken history

--- src/components/YieldHistory.tsx
+++ src/components/YieldHistory.tsx
@@ -2,23 +2,24 @@ import { useContext, useEffect, useState } from 'react';
 import { Box, Collapsible, Text } from 'grommet';
 import Skeleton from 'react-loading-skeleton';
 import { HistoryContext } from '../contexts/HistoryContext';
-import { IBaseHistItem, ISeries, IStrategy, IVault } from '../types';
+import { IBaseHistItem, IHistoryContext, ISeries, IStrategy, IVault } from '../types';
 import EtherscanButton from './buttons/EtherscanButton';
 import { useColorScheme } from '../hooks/useColorScheme';
+import { IVYToken } from '../hooks/entities/useVYTokens';
 
 interface IYieldHistory {
-  seriesOrVault: IVault | ISeries | IStrategy;
-  view: ('STRATEGY' | 'VAULT' | 'TRADE')[];
+  item: IVault | ISeries | IStrategy | IVYToken;
+  view: ('STRATEGY' | 'VAULT' | 'TRADE' | 'VYTOKEN')[];
 }
 
-const YieldHistory = ({ seriesOrVault, view }: IYieldHistory) => {
+const YieldHistory = ({ item, view }: IYieldHistory) => {
   /* STATE FROM CONTEXT */
   const { historyState, historyActions } = useContext(HistoryContext);
   const colorScheme = useColorScheme();
   const color = colorScheme === 'dark' ? 'hoverBackground' : '#f9f9f9';
 
-  const { vaultHistory, tradeHistory, strategyHistory } = historyState;
-  const { updateVaultHistory, updateTradeHistory, updateStrategyHistory } = historyActions;
+  const { vaultHistory, tradeHistory, strategyHistory, vyTokenHistory } = historyState;
+  const { updateVaultHistory, updateTradeHistory, updateStrategyHistory, updateVYTokenHistory } = historyActions;
 
   /* LOCAL STATE */
   const [histList, setHistList] = useState<IBaseHistItem[]>([]);
@@ -26,38 +27,56 @@ const YieldHistory = ({ seriesOrVault, view }: IYieldHistory) => {
   const [histLoading, setHistLoading] = useState<boolean>(true);
 
   useEffect(() => {
-
-
     if (view.includes('VAULT')) {
-      vaultHistory.has(seriesOrVault.id)
-        ? setHistList(vaultHistory.get(seriesOrVault.id))
+      vaultHistory.has(item.id)
+        ? setHistList(vaultHistory.get(item.id))
         : (async () => {
             setHistLoading(true);
-            await updateVaultHistory([seriesOrVault]);
+            await updateVaultHistory([item]);
             setHistLoading(false);
           })();
     }
 
     if (view.includes('STRATEGY')) {
-      strategyHistory.has(seriesOrVault.id)
-        ? setHistList(strategyHistory.get(seriesOrVault.id))
+      strategyHistory.has(item.id)
+        ? setHistList(strategyHistory.get(item.id))
         : (async () => {
             setHistLoading(true);
-            await updateStrategyHistory([seriesOrVault]);
+            await updateStrategyHistory([item]);
             setHistLoading(false);
           })();
     }
 
     if (view.includes('TRADE')) {
-      tradeHistory.has(seriesOrVault.id)
-        ? setHistList(tradeHistory.get(seriesOrVault.id))
+      tradeHistory.has(item.id)
+        ? setHistList(tradeHistory.get(item.id))
         : (async () => {
             setHistLoading(true);
-             await updateTradeHistory([seriesOrVault]);
+            await updateTradeHistory([item]);
             setHistLoading(false);
           })();
     }
-  }, [seriesOrVault, strategyHistory, tradeHistory, vaultHistory, view]);
+    if (view.includes('VYTOKEN')) {
+      vyTokenHistory?.has(item.id)
+        ? setHistList(vyTokenHistory.get(item.id)!)
+        : (async () => {
+            setHistLoading(true);
+            await updateVYTokenHistory([item.id]);
+            setHistLoading(false);
+          })();
+    }
+  }, [
+    item,
+    strategyHistory,
+    tradeHistory,
+    updateStrategyHistory,
+    updateTradeHistory,
+    updateVYTokenHistory,
+    updateVaultHistory,
+    vaultHistory,
+    view,
+    vyTokenHistory,
+  ]);
 
   return (
     <Box margin={{ top: 'medium' }} height={{ max: '200px' }} style={{ overflow: 'auto' }}>
@@ -83,18 +102,18 @@ const YieldHistory = ({ seriesOrVault, view }: IYieldHistory) => {
               >
                 <Box direction="row">
                   <Box basis="25%">
-                    <Text size="xsmall"> {x.date_}</Text>
+                    <Text size="xsmall">{x.date_}</Text>
                   </Box>
                   <Box direction="row" fill justify="between">
                     <Text size="xsmall" weight={900}>
                       {x.actionCode}
                     </Text>
-                    <Text size="xsmall"> {x.primaryInfo} </Text>
+                    <Text size="xsmall">{x.primaryInfo}</Text>
                   </Box>
                 </Box>
                 <Collapsible open={itemOpen === key_}>
                   <Box direction="row" justify="between">
-                    <Text size="xsmall"> {x.secondaryInfo} </Text>
+                    <Text size="xsmall">{x.secondaryInfo}</Text>
                     <EtherscanButton txHash={x.transactionHash} />
                   </Box>
                 </Collapsible>
