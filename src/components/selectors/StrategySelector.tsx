3dd82887cefb7a4bd6c5a4b4410e3a206fe488e6
Jacob Bryant <jacobbryant@Jacobs-MBP.attlocal.net>
2023-05-26 11:28:11 -0400

    showing just v2.1 strats

--- src/components/selectors/StrategySelector.tsx
+++ src/components/selectors/StrategySelector.tsx
@@ -140,10 +140,11 @@ const StrategySelector = ({ inputValue }: IStrategySelectorProps) => {
   useEffect(() => {
     const opts = Array.from(strategyMap?.values()!);
     const filteredOpts = opts
-      .filter((_st) => _st.type === 'V2.1' || _st.type === 'V2' || (_st.type === 'V1' && !_st.associatedStrategy))
+      .filter((_st) => _st.type === 'V2.1')
       .filter((_st) => !_st.currentSeries?.hideSeries && _st.active)
       .filter((_st) => _st.baseId === selectedBase?.proxyId && !_st.currentSeries?.seriesIsMature)
       .sort((a, b) => a.currentSeries?.maturity! - b.currentSeries?.maturity!);
+
     setOptions(filteredOpts);
   }, [selectedBase, strategyMap, selectedStrategy]);
 
