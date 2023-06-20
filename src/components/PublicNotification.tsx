aebca4096918ca9e4b1ac9b5e0629034b2cb8ab7
marcomariscal <marco.a.mariscal@gmail.com>
2023-06-20 10:15:14 -0700

    feat: style

--- src/components/PublicNotification.tsx
+++ src/components/PublicNotification.tsx
@@ -24,15 +24,21 @@ const PublicNotification = ({ children }: PublicNotificationProps) => {
       {chainId === 1 && hasUpgradeable ? (
         <Box direction="row" align="center" justify="between">
           <Box direction="column" border={{ size: 'small' }} pad="small" gap="small" align="center" round="xsmall">
-            <Box direction="row" gap="small">
-              <Text size="large">
-                <FiAlertTriangle />
-              </Text>
-              <Text size="xsmall">
-                Action Required: As a consequence of the Euler hack, your strategy tokens need to be upgraded
+            <Box gap="small" align="center">
+              <Box direction="row" gap="xsmall" align="center">
+                <Text size="medium">
+                  <FiAlertTriangle />
+                </Text>
+                <Text size="medium" weight={'bold'}>
+                  Action Required
+                </Text>
+              </Box>
+              <Text size="small" textAlign="center">
+                As a consequence of the Euler hack, your strategy tokens need to be upgraded
               </Text>
             </Box>
             <Button
+              fill
               label="Upgrade"
               onClick={() => {
                 showTermsModal();
