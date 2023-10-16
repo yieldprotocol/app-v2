b85ce4ae4902d861d61f79464833ec8e4cd60285
brucedonovan <bruce.donovan@gmail.com>
2023-10-16 11:18:36 +0100

    removeMiddelware

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-
+function randomNonce(length = 32) {
+  let nonce = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+      nonce += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return nonce;
+}
 
 export default class MyDocument extends Document {
   render() {
+    const nonce = randomNonce();
+    const csp = `object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http: 'nonce-${nonce}' 'strict-dynamic'`
+
     return (
       <Html>
         <Head>
@@ -12,11 +23,12 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
           <meta httpEquiv='X-Frame-Options' content='DENY' />
         </Head>
         <body>
           <Main />
-          <NextScript />
+          <NextScript nonce={nonce} />
         </body>
       </Html>
     );
