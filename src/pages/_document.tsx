bb56acffcea73e12b0c7153230d144a1ec8ae338
brucedonovan <bruce.donovan@gmail.com>
2023-10-16 11:27:41 +0100

    NonceInHead

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,12 +17,13 @@ export default class MyDocument extends Document {
 
     return (
       <Html>
-        <Head>
+        <Head nonce={nonce}>
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;0,900;1,100;1,400;1,700;1,900&display=optional"
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta property='csp-nonce' content={nonce} />
           <meta httpEquiv="Content-Security-Policy" content={csp} />
           <meta httpEquiv='X-Frame-Options' content='DENY' />
         </Head>
